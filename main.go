64e6f2b76e146ada95100dfa692e9090a8fcd31a
Sammy <mamxxim@163.com>
2016-08-18 13:50:42 +0800

    指定静态文件目录

--- main.go
+++ main.go
@@ -89,7 +89,8 @@ func main(){
 			//实例化路由
 			router := routers.NewRouter()
 			// 指定静态文件目录
-			http.Handle("/static/", http.StripPrefix("/static/",http.FileServer(http.Dir("./static"))))
+			router.PathPrefix("/").Handler(http.FileServer(http.Dir(".")))
+
 			//启动http服务
 			ctx.String("启动http服务...\n")
 			log.Fatal(http.ListenAndServe(":"+strconv.Itoa(argv.HttpServerPORT),router))
