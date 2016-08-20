ff748e6a2135dba4084c65dec323a75a99c8acaf
snailset <snailset@163.com>
2016-08-20 14:21:26 +0800

    change

--- core/txpool_mem.go
+++ core/txpool_mem.go
@@ -1,1 +1,24 @@
 package core
+
+import (
+	"sync"
+	"hyperchain-alpha/core/types"
+)
+
+type memTxPool struct {
+	txPool types.TxPool
+	lock sync.RWMutex
+}
+
+var memTxPoolMap *memTxPool;
+
+func GetTxPool() types.TxPool {
+	memChainMap.lock.Lock()
+	defer memChainMap.lock.Unlock()
+	return memTxPoolMap.txPool
+}
+
+
+func AddTransactionToTxPool(tran types.Transaction) {
+	memTxPoolMap.txPool.Transactions = append(memTxPoolMap.txPool.Transactions, tran)
+}
