304d246ff0942350170a30d7ae1f371ffbbf2bae
chenquan <quan.chen@hyperchain.cn>
2016-08-20 13:47:51 +0800

    删除了model的遗留

--- jsonrpc/controller/indexController.go
+++ jsonrpc/controller/indexController.go
@@ -3,6 +3,8 @@ import (
 	"net/http"
 	"html/template"
 	"hyperchain-alpha/core"
+	"hyperchain-alpha/utils"
+	"path"
 )
 
 //type Transaction struct{
@@ -14,12 +16,13 @@ import (
 //
 //type Transacctions []Transaction
 
-var path string = "./static/tmpl/"
+
 
 // 处理请求 : GET "/"
 func Index(w http.ResponseWriter, r *http.Request) {
+	indexpath := path.Join(utils.GetBasePath(),"./jsonrpc/static/tmpl/index.html")
 
-	var tmpl = template.Must(template.ParseFiles(path+"index.html"))
+	var tmpl = template.Must(template.ParseFiles(indexpath))
 
 	//TODO 读取keystore文件夹，包括公钥、私钥、用户名，有个用户名与私钥的map对应表 map[string]string
 
