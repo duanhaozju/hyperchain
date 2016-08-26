c40b0f42ea460c973630efefe3e0105762119374
snailset <snailset@163.com>
2016-08-26 16:05:14 +0800

    database

--- hyperdb/database.go
+++ hyperdb/database.go
@@ -3,72 +3,124 @@ package hyperdb
 import (
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/iterator"
+	"sync"
 )
 
-
-//-- 用leveldb实现的LDBDatabase
-//-- LDBDatabase实现了DataBase接口
-
+// the Database for LevelDB
+// LDBDatabase implements the DataBase interface
 type LDBDatabase struct {
-	filepath string
-	db *leveldb.DB
+	path string
+	db   *leveldb.DB
+	state statedb
+	dbsync sync.Mutex
 }
 
+type statedb int32
+
+const (
+	closed statedb  = iota
+	opened
+)
+
+var ldbDatabase = &LDBDatabase{
+	state: closed,
+}
+
+func getDBPath() string {
+	return ""
+}
+
+// GetLDBDatabase get a single instance of LDBDatabase
+// if LDBDatabase state is open, return db directly
+// if LDBDatabase state id close,
+func GetLDBDatabase() (*LDBDatabase, error) {
+	ldbDatabase.dbsync.Lock()
+	defer ldbDatabase.dbsync.Unlock()
+	if ldbDatabase.state == opened {
+		return ldbDatabase, nil
+	}
+	db, err := leveldb.OpenFile(getDBPath(), nil)
+	if err != nil {
+		return ldbDatabase, err
+	}
+	ldbDatabase.db = db
+	ldbDatabase.path = getDBPath()
+	ldbDatabase.state = opened
+	return ldbDatabase, nil
+}
+
+// NewLDBDataBase new a LDBDatabase instance
+// require a data filepath
+// return *LDBDataBase and  will
+// return an error with type of
+// ErrCorrupted if corruption detected in the DB. Corrupted
+// DB can be recovered with Recover function.
+// the return *LDBDatabase is goruntine-safe
+// the LDBDataBase instance must be close after use, by calling Close method
 func NewLDBDataBase(filepath string) (*LDBDatabase, error) {
-	DBLog("new a ldb database connect")
-	db, err := leveldb.OpenFile(filepath, nil )
+	db, err := leveldb.OpenFile(filepath, nil)
 	return &LDBDatabase{
-		filepath:filepath,
-		db:db,
+		path: filepath,
+		db: db,
 	}, err
 }
 
-func (self *LDBDatabase) Put(key []byte, value []byte) error {
 
-	DBLog("put data to ldb")
+
+// Put sets value for the given key, if the key exists, it will overwrite
+// the value
+func (self *LDBDatabase) Put(key []byte, value []byte) error {
 	return self.db.Put(key, value, nil)
 }
 
+// Get gets value for the given key, it returns ErrNotFound if
+// the Database does not contains the key
 func (self *LDBDatabase) Get(key []byte) ([]byte, error) {
-	DBLog("get data from ldb")
 	dat, err := self.db.Get(key, nil)
 	return dat, err
 }
 
+// Delete deletes the value for the given key
 func (self *LDBDatabase) Delete(key []byte) error {
-	DBLog("delete data")
 	return self.db.Delete(key, nil)
 }
 
+// NewIterator returns a Iterator for traversing the database
 func (self *LDBDatabase) NewIterator() iterator.Iterator {
 	return self.db.NewIterator(nil, nil)
 }
 
+// Close close the LDBDataBase
 func (self *LDBDatabase) Close() {
-	DBLog("close the ldb connect")
 	self.db.Close()
 }
 
+// LDB returns *leveldb.DB instance
 func (self *LDBDatabase) LDB() *leveldb.DB {
 	return self.db
 }
 
+// NewBatch returns a Batch instance
+// it allows batch-operation
 func (db *LDBDatabase) NewBatch() Batch {
 	return &ldbBatch{db: db.db, b: new(leveldb.Batch)}
 }
 
 
-//-- 数据库的batch操作
+// The Batch for LevelDB
+// ldbBatch implements the Batch interface
 type ldbBatch struct {
 	db *leveldb.DB
 	b  *leveldb.Batch
 }
 
+// Put put the key-value to ldbBatch
 func (b *ldbBatch) Put(key, value []byte) error {
 	b.b.Put(key, value)
 	return nil
 }
 
+// Write write batch-operation to databse
 func (b *ldbBatch) Write() error {
 	return b.db.Write(b.b, nil)
 }
