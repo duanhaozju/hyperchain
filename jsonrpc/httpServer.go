8b28f049b5a5884a7b3307a090ca5bfb3382f2f9
Sammy <mamxxim@163.com>
2016-08-29 21:59:02 +0800

    修改提示消息

--- jsonrpc/httpServer.go
+++ jsonrpc/httpServer.go
@@ -11,9 +11,9 @@ func StartHttp(httpPort int){
 	//实例化路由
 	router := routers.NewRouter()
 	// 指定静态文件目录
-	router.PathPrefix("/").Handler(http.FileServer(http.Dir(".")))
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./jsonrpc")))
 
 	//启动http服务
-		log.Println("启动http服务...")
+	log.Println("启动http服务...")
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(httpPort),router))
 }
