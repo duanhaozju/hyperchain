a9e41ea3a6be5e44879594dc493e99a028e6b3b2
Sammy <mamxxim@163.com>
2016-08-30 20:25:28 +0800

    修改api包名等

---
+++ jsonrpc/api/api_test.go
@@ -0,0 +1,66 @@
+package api
+
+import (
+	"testing"
+	"hyperchain/core"
+	"log"
+	"hyperchain/common"
+	"hyperchain/manager"
+	"hyperchain/p2p"
+	"hyperchain/event"
+	"hyperchain/crypto"
+	"github.com/stretchr/testify/assert"
+)
+
+//var pm *manager.ProtocolManager
+
+func initPM() *manager.ProtocolManager{
+	eventMux := new(event.TypeMux)
+	peerManager := new(p2p.GrpcPeerManager)
+	fetcher := core.NewFetcher()
+	encryption :=crypto.NewEcdsaEncrypto("ecdsa")
+	encryption.GeneralKey(string(8012))
+	commonHash:=crypto.NewKeccak256Hash("keccak256")
+
+	return manager.NewProtocolManager(peerManager, eventMux, fetcher, nil, encryption, commonHash)
+}
+
+func TestSendTransaction(t *testing.T) {
+
+	initPM()
+
+	isSuccess := SendTransaction(TxArgs{
+		From: "addressFrom",
+		To: "addressTo",
+		Value: "12",
+	})
+
+	assert.Equal(t,false, isSuccess, "they should be equal")
+
+	balanceIns,err := core.GetBalanceIns()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	balanceIns.PutCacheBalance(common.BytesToAddress([]byte("addressFrom")),[]byte("13"))
+
+	isSuccess2 := SendTransaction(TxArgs{
+		From: "addressFrom",
+		To: "addressTo",
+		Value: "12",
+	})
+
+	assert.Equal(t,true, isSuccess2, "they should be equal")
+}
+
+//func TestGetAllTransactions(t *testing.T) {
+//
+//	tx := types.NewTransaction([]byte("addressFrom"),[]byte("addressTo"),[]byte("12"))
+//	tx.TimeStamp = time.Now().Unix()
+//
+//	pm := initPM()
+//
+//	tx.SighHash(pm.commonHash)
+//
+//}
+
+
--- jsonrpc/controller/indexController.go
+++ jsonrpc/controller/indexController.go
@@ -3,8 +3,8 @@ import (
 	"net/http"
 	"html/template"
 	"path"
+	"hyperchain/jsonrpc/api"
 	"os"
-	"hyperchain/jsonrpc/hyperchain"
 )
 
 
@@ -14,22 +14,32 @@ type ResData struct{
 }
 
 type data struct{
-	Trans []hyperchain.Transaction
-	Balances hyperchain.Balance
+	Trans    []api.TransactionShow
+	Balances api.BalanceShow
 }
+
+var Testing bool = false
 
 // 处理请求 : GET "/"
 func Index(w http.ResponseWriter, r *http.Request) {
+
+	var indexpath string
+
 	pwd, _ := os.Getwd()
-	indexpath := path.Join(pwd,"./jsonrpc/static/tmpl/index.html")
+
+	if Testing {
+		indexpath = path.Join(pwd,"./static/tmpl/index.html")
+	} else {
+		indexpath = path.Join(pwd,"./jsonrpc/static/tmpl/index.html")
+	}
+
 
 	var tmpl = template.Must(template.ParseFiles(indexpath))
 
-	transactions := hyperchain.GetAllTransactions()
-	balances := hyperchain.GetAllBalances()
+	transactions := api.GetAllTransactions()
+	balances := api.GetAllBalances()
 
 	//sort.Sort(types.Transactions(transactions)) // 交易排序
-
 
 	tmpl.Execute(w,data{
 		Trans:transactions,
