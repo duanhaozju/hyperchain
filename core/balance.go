c941425695f7ee90b61f73d69d868b2589f8386a
snailset <snailset@163.com>
2016-09-01 19:39:59 +0800

    change notes as english

--- core/balance.go
+++ core/balance.go
@@ -27,6 +27,7 @@ type Balance struct {
 	dbBalance    BalanceMap   // store in db, synchronization with block
 	cacheBalance BalanceMap   // synchronization with transaction
 	lock         sync.RWMutex // the lock for balance of reading of writing
+
 	state        stateType    // the balance state, use for singleton
 	stateLock    sync.Mutex   // the lock of get balance instance
 }
@@ -35,10 +36,11 @@ var balance = &Balance{
 	state: closed,
 }
 
-// GetBalanceIns 获取Balance单例
-// 如果没有创建实例，册创建，创建过程：
-// 先从数据库中读，如果读到，则将dbBalance，cacheBalance都赋值为读到的Map
-// 如果数据库中没有，则创建一个空的dbBalance和cacheBalance
+// GetBalanceIns get balance singleton instance
+// if there is no balance instance, it will create one. creating process:
+// read dbBalance from db firstly, if exists, dbBalance and cacheBalance
+// will be assigned dbBalance which get from db. if not exists,
+// it will create empty dbBalance and cacheBalance
 func GetBalanceIns() (*Balance, error) {
 	balance.stateLock.Lock()
 	defer balance.stateLock.Unlock()
@@ -67,30 +69,29 @@ func GetBalanceIns() (*Balance, error) {
 	return balance, nil
 }
 
-//var balanceInstance = newBalanceIns()
 
-//-- 将Balance存入内存
-func (self *Balance)PutCacheBalance(accountPublicKeyHash common.Address, value []byte) {
+//-- PutCacheBalance put cacheBalance(just into memory not db)
+func (self *Balance)PutCacheBalance(address common.Address, value []byte) {
 	self.lock.Lock()
 	defer self.lock.Unlock()
-	self.cacheBalance[accountPublicKeyHash] = value
+	self.cacheBalance[address] = value
 }
 
-//-- 在MEM中 根据Key获取的Balance
-func (self *Balance)GetCacheBalance(accountPublicKeyHash common.Address) []byte {
+//-- GetCacheBalance get cacheBalance by address
+func (self *Balance)GetCacheBalance(address common.Address) []byte {
 	self.lock.RLock()
 	defer self.lock.RUnlock()
-	return self.cacheBalance[accountPublicKeyHash]
+	return self.cacheBalance[address]
 }
 
-//-- 从MEM中删除Balance
-func (self *Balance)DeleteCacheBalance(accountPublicKeyHash common.Address) {
+//-- DeleteCacheBalance delete cacheBalance for given address
+func (self *Balance)DeleteCacheBalance(address common.Address) {
 	self.lock.Lock()
 	defer self.lock.Unlock()
-	delete(self.cacheBalance, accountPublicKeyHash)
+	delete(self.cacheBalance, address)
 }
 
-//-- 从MEM中获取所有Balance
+//-- GetAllCacheBalance get all cacheBalance
 func (self *Balance)GetAllCacheBalance() (BalanceMap) {
 	self.lock.RLock()
 	defer self.lock.RUnlock()
@@ -101,28 +102,28 @@ func (self *Balance)GetAllCacheBalance() (BalanceMap) {
 	return bs
 }
 
-//-- 将Balance存入内存
-func (self *Balance)PutDBBalance(accountPublicKeyHash common.Address, value []byte) {
+//-- PutDBBalance put dbbalance (just into memory not db)
+func (self *Balance)PutDBBalance(address common.Address, value []byte) {
 	self.lock.Lock()
 	defer self.lock.Unlock()
-	self.dbBalance[accountPublicKeyHash] = value
+	self.dbBalance[address] = value
 }
 
-//-- 在MEM中 根据Key获取的Balance
-func (self *Balance)GetDBBalance(accountPublicKeyHash common.Address) []byte {
+//-- GetDBBalance get dbBalance by address
+func (self *Balance)GetDBBalance(address common.Address) []byte {
 	self.lock.RLock()
 	defer self.lock.RUnlock()
-	return self.dbBalance[accountPublicKeyHash]
+	return self.dbBalance[address]
 }
 
-//-- 从MEM中删除Balance
-func (self *Balance)DeleteDBBalance(accountPublicKeyHash common.Address) {
+//-- DeleteDBBalance deleate dbBalance for given address
+func (self *Balance)DeleteDBBalance(address common.Address) {
 	self.lock.Lock()
 	defer self.lock.Unlock()
-	delete(self.dbBalance, accountPublicKeyHash)
+	delete(self.dbBalance, address)
 }
 
-//-- 从MEM中获取所有Balance
+//-- GetAllDBBalance get all dbBalance
 func (self *Balance)GetAllDBBalance() (BalanceMap) {
 	self.lock.RLock()
 	defer self.lock.RUnlock()
@@ -142,26 +143,21 @@ func (self *Balance)UpdateDBBalance(block *types.Block) error {
 		return err
 	}
 	for _, trans := range block.Transactions {
-		//-- 将交易里的From(账户的publickey)余额减去value
-		//-- 如果余额表中没有这个From(实际上不可能，因为余额表中没有这个From，不可能会验证通过
-		//-- 但是上帝用户例外，因为上帝用户可能会出现负数)，则新建一个
-		//-- 如果余额表中有这个From，则覆盖publickey(覆盖的Publickey是一样的，实际上没改)
 		var transValue big.Int
 		transValue.SetString(string(trans.Value), 10)
 		fromBalance := self.dbBalance[common.BytesToAddress(trans.From)]
 		toBalance := self.dbBalance[common.BytesToAddress(trans.To)]
 		var fromValue big.Int
 		var toValue big.Int
-
 		fromValue.SetString(string(fromBalance), 10)
 		toValue.SetString(string(toBalance), 10)
-		//b -= value
 		fromValue.Sub(&fromValue, &transValue)
+
+		// Update Transaction.From account(sub the From account balance by value)
 		self.dbBalance[common.BytesToAddress(trans.From)] = []byte(fromValue.String())
-		//-- 将交易中的To(账户中的publickey)余额加上value
-		//-- 如果余额表中没有这个To(就是所有publickey不含有To)
-		//-- 新建一个balance，将交易的value直接赋给balance.value
-		//-- 如果余额表中有这个To,则直接加上交易中的value
+
+		// Update Transaction.To account(add the To account balance by value)
+		// if Transaction.To account not exist, it will be created, initial account balance is 0
 		if _, ok := self.dbBalance[common.BytesToAddress(trans.To)]; ok {
 			toValue.Add(&toValue, &transValue)
 			self.dbBalance[common.BytesToAddress(trans.To)] = []byte(toValue.String())
@@ -188,11 +184,14 @@ func (self *Balance)UpdateCacheBalance(trans *types.Transaction) {
 	toBalance := self.cacheBalance[common.BytesToAddress(trans.To)]
 	var fromValue big.Int
 	var toValue big.Int
-
 	fromValue.SetString(string(fromBalance), 10)
 	toValue.SetString(string(toBalance), 10)
-	//b -= value
+
+	// Update Transaction.From account(sub the From account balance by value)
 	fromValue.Sub(&fromValue, &transValue)
+
+	// Update Transaction.To account(add the To account balance by value)
+	// if Transaction.To account not exist, it will be created, initial account balance is 0
 	self.cacheBalance[common.BytesToAddress(trans.From)] = []byte(fromValue.String())
 	if _, ok := self.cacheBalance[common.BytesToAddress(trans.To)]; ok {
 		toValue.Add(&toValue, &transValue)
@@ -205,7 +204,7 @@ func (self *Balance)UpdateCacheBalance(trans *types.Transaction) {
 
 // VerifyTransaction is to verify balance of the tranaction
 // If the balance is not enough, returns false
-func VerifyBalance(tx *types.Transaction) bool{
+func VerifyBalance(tx *types.Transaction) bool {
 	var balance big.Int
 	var value big.Int
 
