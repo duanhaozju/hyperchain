166d885c383fd5abac0dd1a3001deae96aa1bc46
kuang <472076043@qq.com>
2016-09-04 20:26:33 +0800

    fix send tx time

--- core/database_calc.go
+++ core/database_calc.go
@@ -3,6 +3,7 @@ package core
 import (
 	"hyperchain/hyperdb"
 	"time"
+	"fmt"
 )
 
 // CalcResponseCount calculate response count of a block for given blockNumber
@@ -13,9 +14,16 @@ func CalcResponseCount(blockNumber uint64, millTime int64) int64 {
 		log.Fatal(err)
 	}
 	blockHash, err := GetBlockHash(db, blockNumber)
+	fmt.Println(blockHash)
 	block, err := GetBlock(db, blockHash)
 	var count int64 = 0
-	for _, trans := range block.Transactions {
+	for i, trans := range block.Transactions {
+
+		fmt.Println(time.Unix(block.WriteTime/1e9, 0).Format("2006-01-02 03:04:05 PM"))
+		fmt.Println(time.Unix(trans.TimeStamp/1e9, 0).Format("2006-01-02 03:04:05 PM"))
+
+		fmt.Println(i , " : ", block.Timestamp - trans.TimeStamp)
+		fmt.Println(i , " : ", block.WriteTime - trans.TimeStamp)
 		if block.WriteTime - trans.TimeStamp <= millTime * int64(time.Millisecond) {
 			count ++
 		}
