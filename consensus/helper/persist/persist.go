a9ffb6825a078ca4260c989cf56da4bf7797c6a1
zarczhao <zarc@zju.edu.cn>
2016-09-06 10:45:42 +0800

    uncomment persistDelAllRequestBatches()

--- consensus/helper/persist/persist.go
+++ consensus/helper/persist/persist.go
@@ -1,26 +1,14 @@
 package persist
 
 import (
-	//"fmt"
-	//"bytes"
-
+	"fmt"
 	"hyperchain/hyperdb"
 	"hyperchain/core"
+	"bytes"
+	"github.com/pkg/errors"
 	"hyperchain/core/types"
-
-	//"github.com/pkg/errors"
 )
 
-// Helper provides an abstraction to access the Persist column family
-// in the database.
-//type Helper struct{}
-//
-//type persistStack interface {
-//	StoreState(key string, value []byte) error
-//	DelState(key string) error
-//	ReadState(key string) ([]byte, error)
-//	ReadStateSet(prefix string) (map[string][]byte, error)
-//}
 
 // StoreState stores a key,value pair
 func StoreState(key string, value []byte) error {
@@ -41,27 +29,28 @@ func ReadState(key string) ([]byte, error) {
 	return db.Get([]byte("consensus."+key))
 }
 
-//// ReadStateSet retrieves all key-value pairs where the key starts with prefix
-//func ReadStateSet(prefix string) (map[string][]byte, error) {
-//	db, _ := hyperdb.GetLDBDatabase()
-//	prefixRaw := []byte("consensus." + prefix)
-//
-//	ret := make(map[string][]byte)
-//	it := db.NewIterator()
-//	defer it.Release()
-//	if !it.Seek(prefixRaw) {
-//		err := errors.New(fmt.Sprintf("Cannot find key with %s in database", prefixRaw))
-//		return nil, err
-//	}
-//	for ; bytes.HasPrefix(it.Key(), prefixRaw); it.Next() {
-//		key := it.Key()
-//		key = key[len("consensus."):]
-//		ret[key] = append([]byte(nil), it.Value())
-//	}
-//	return ret, nil
-//}
+// ReadStateSet retrieves all key-value pairs where the key starts with prefix
+func ReadStateSet(prefix string) (map[string][]byte, error) {
+	db, _ := hyperdb.GetLDBDatabase()
+	prefixRaw := []byte("consensus." + prefix)
+
+	ret := make(map[string][]byte)
+	it := db.NewIterator()
+	defer it.Release()
+	if !it.Seek(prefixRaw) {
+		err := errors.New(fmt.Sprintf("Cannot find key with %s in database", prefixRaw))
+		return nil, err
+	}
+	for ; bytes.HasPrefix(it.Key(), prefixRaw); it.Next() {
+		key := string(it.Key())
+		key = key[len("consensus."):]
+		ret[key] = append([]byte(nil), it.Value()...)
+	}
+	return ret, nil
+}
 
 func GetBlockchainInfo() *types.Chain {
 	bcInfo := core.GetChainCopy()
+
 	return bcInfo
 }
