78de7b4cb6ceaf8f7dce34838825298a53a62e68
snailset <snailset@163.com>
2016-09-07 14:24:10 +0800

    modify test

--- jsonrpc/api/api_test.go
+++ jsonrpc/api/api_test.go
@@ -29,6 +29,12 @@ func TestSendTransaction(t *testing.T) {
 
 	initPM()
 
+	balanceIns,err := core.GetBalanceIns()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	balanceIns.DeleteCacheBalance(common.BytesToAddress([]byte("000000000000000000000000000000adressFrom")))
+
 	isSuccess := SendTransaction(TxArgs{
 		From: "000000000000000000000000000000adressFrom",
 		To: "00000000000000000000000000000000adressTo",
@@ -36,10 +42,7 @@ func TestSendTransaction(t *testing.T) {
 	})
 	assert.Equal(t,false, isSuccess, "they should be equal")
 
-	balanceIns,err := core.GetBalanceIns()
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
+
 	balanceIns.PutCacheBalance(common.BytesToAddress([]byte("000000000000000000000000000000adressFrom")),[]byte("13"))
 
 	isSuccess2 := SendTransaction(TxArgs{
