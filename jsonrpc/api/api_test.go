e078409eb8657f081a10389211b9f0c3d1c90459
chenquan <quan.chen@hyperchain.cn>
2016-09-07 20:30:32 +0800

    change api_test initPM function

--- jsonrpc/api/api_test.go
+++ jsonrpc/api/api_test.go
@@ -4,39 +4,11 @@ import (
 	"testing"
 	"hyperchain/core"
 	"hyperchain/common"
-	"hyperchain/manager"
-	"hyperchain/p2p"
-	"hyperchain/event"
-	"hyperchain/crypto"
 	"github.com/stretchr/testify/assert"
-	"hyperchain/accounts"
 )
 
-//var pm *manager.ProtocolManager
-
-func initPM() *manager.ProtocolManager{
-	eventMux := new(event.TypeMux)
-	peerManager := new(p2p.GrpcPeerManager)
-	fetcher := core.NewFetcher()
-	commonHash:=crypto.NewKeccak256Hash("keccak256")
-	//blockPool:=core.NewBlockPool(eventMux)
-
-	//init encryption object
-	encryption := crypto.NewEcdsaEncrypto("ecdsa")
-	encryption.GeneralKey("8012")
-
-	scryptN := accounts.StandardScryptN
-	scryptP := accounts.StandardScryptP
-	keydir := "../keystore/"
-	am := accounts.NewAccountManager(keydir,encryption, scryptN, scryptP)
-
-	return manager.NewProtocolManager(nil, peerManager, eventMux, fetcher, nil, am, commonHash)
-}
 
 func TestSendTransaction(t *testing.T) {
-
-	initPM()
-
 	balanceIns,err := core.GetBalanceIns()
 	if err != nil {
 		log.Fatalf("%v", err)
@@ -61,16 +33,4 @@ func TestSendTransaction(t *testing.T) {
 
 	assert.Equal(t,true, isSuccess2, "they should be equal")
 }
-
-//func TestGetAllTransactions(t *testing.T) {
-//
-//	tx := types.NewTransaction([]byte("00000000000000000000000000000adressFrom"),[]byte("addressTo"),[]byte("12"))
-//	tx.TimeStamp = time.Now().Unix()
-//
-//	pm := initPM()
-//
-//	tx.SighHash(pm.commonHash)
-//
-//}
-
 
