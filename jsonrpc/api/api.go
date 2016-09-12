8ff3036d95e8c0d0bc1eb15c51bb471c9e4b89c8
sylvia@lab_ubuntu <hfanglei@gmail.com>
2016-09-12 19:28:01 +0800

    test 600 second

--- jsonrpc/api/api.go
+++ jsonrpc/api/api.go
@@ -67,7 +67,7 @@ func SendTransaction(args TxArgs) bool {
 
 		log.Infof("############# %d: start send request#############", time.Now().Unix())
 		start := time.Now().Unix()
-		end:=start+6
+		end:=start+60
 
 		for start := start ; start < end; start = time.Now().Unix() {
 			for i := 0; i < 5000; i++ {
@@ -81,7 +81,7 @@ func SendTransaction(args TxArgs) bool {
 				}else{
 					log.Warning("manager is Nil")
 				}
-				time.Sleep(2 * time.Millisecond)
+				time.Sleep(200 * time.Nanosecond)
 			}
 		}
 
