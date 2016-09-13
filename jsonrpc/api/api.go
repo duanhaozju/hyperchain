a9171af7cbcd356e5de3ef35f0dd38a241674102
sylvia@lab_ubuntu <hfanglei@gmail.com>
2016-09-13 15:54:27 +0800

    test without read blockinfo

--- jsonrpc/api/api.go
+++ jsonrpc/api/api.go
@@ -85,7 +85,7 @@ func SendTransaction(args TxArgs) bool {
 				}
 				//time.Sleep(2 * time.Nanosecond)
 			}
-			time.Sleep(50 * time.Millisecond)
+			time.Sleep(80 * time.Millisecond)
 		}
 
 		log.Infof("############# %d: end send request#############", time.Now().Unix())
