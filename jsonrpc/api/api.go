a0b31f1a20f6811e5918ec256bc4b9e35e30ca0f
sylvia@lab_ubuntu <hfanglei@gmail.com>
2016-09-14 20:00:56 +0800

    test

--- jsonrpc/api/api.go
+++ jsonrpc/api/api.go
@@ -72,7 +72,7 @@ func SendTransaction(args TxArgs) bool {
 		end:=start+300
 
 		for start := start ; start < end; start = time.Now().Unix() {
-			for i := 0; i < 200; i++ {
+			for i := 0; i < 120; i++ {
 				tx.TimeStamp=time.Now().UnixNano()
 				txBytes, err := proto.Marshal(tx)
 				if err != nil {
