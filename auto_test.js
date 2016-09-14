5ed6c545a8515d0ac0494943fea0b5c585dac102
sylvia@lab_ubuntu <hfanglei@gmail.com>
2016-09-14 19:02:44 +0800

    test

--- auto_test.js
+++ auto_test.js
@@ -15,7 +15,7 @@
  var params = {form:"",to:"",value:1}
  var hosts_url = []
  var hosts_port = []
- for (var i=1;i<=4;i++){
+ for (var i=1;i<=10;i++){
     hosts_url.push(config['external_node'+i])
     hosts_port.push(config['external_port'+i])
  }
