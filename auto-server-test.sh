c803bead7d5c64a2a974dbe801c0df4b42cd8647
sylvia@lab_ubuntu <hfanglei@gmail.com>
2016-09-17 21:51:13 +0800

    100 nodes test

--- auto-server-test.sh
+++ auto-server-test.sh
@@ -6,12 +6,6 @@ done < ./serverlist.txt
 ni=1
 for server_address in ${SERVER_ADDR[@]}; do
   ni=`expr $ni + 1`
-  # gnome-terminal -x bash -c "ssh satoshi@$server_address \"cd /home/satoshi/gopath/src/hyperchain/scripts/ && ./server.sh $i\""
-  ssh -t satoshi@$server_address "
-   cd /home/satoshi/gopath/src/hyperchain/scripts && \
-   git checkout -- server.sh && \
-   git pull origin develop && \
-   pwd && \
-   chmod a+x server.sh && \
-   ./server.sh $ni"
+  gnome-terminal -x bash -c "ssh -t satoshi@$server_address \"cd /home/satoshi/gopath/src/hyperchain/scripts && git checkout -- server.sh && git pull origin develop && pwd && chmod a+x server.sh && ./server.sh $ni\""
+#   ssh -t satoshi@$server_address "cd /home/satoshi/gopath/src/hyperchain/scripts && git checkout -- server.sh && git pull origin develop && pwd && chmod a+x server.sh && ./server.sh $ni"
 done
