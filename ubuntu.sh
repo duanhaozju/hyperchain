0bb4c78425732f553ae88abde6d9c9e70b6b1f6c
sylvia@lab_ubuntu <hfanglei@gmail.com>
2016-09-17 21:12:22 +0800

    100 nodes test

--- ubuntu.sh
+++ ubuntu.sh
@@ -21,77 +21,73 @@ PASSWD="blockchain"
 #kill the progress
 kellprogress(){
 
-   echo "kill the bind port process"
-   for((i=1;i<=$MAXNODE;i++))
-   do
-       temp_port=`lsof -i :800$i | awk 'NR>=2{print $2}'`
-       if [ x"$temp_port" != x"" ];then
-           kill -9 $temp_port
-       fi
-   done
+  echo "kill the bind port process"
+  for((i=1;i<=$MAXNODE;i++))
+  do
+      temp_port=`lsof -i :800$i | awk 'NR>=2{print $2}'`
+      if [ x"$temp_port" != x"" ];then
+          kill -9 $temp_port
+      fi
+  done
 }
 
 local_test(){
-   kellprogress
-   CONFIG_PATH=$1
-   #rebuild the application
-   echo "rebuild the application"
-   govendor build
+  kellprogress
+  CONFIG_PATH=$1
+  #rebuild the application
+  echo "rebuild the application"
+  govendor build
 
 
-   echo "remove tmp data"
-   rm -rf /tmp/hyperchain/*
+  echo "remove tmp data"
+  rm -rf /tmp/hyperchain/*
 
-   echo "running the application"
+  echo "running the application"
 
-   for((j=1;j<=$MAXNODE;j++))
-   do
-       gnome-terminal -x bash -c "(./hyperchain -o $j -l 808$j -p $CONFIG_PATH)"
-   done
+  for((j=1;j<=$MAXNODE;j++))
+  do
+      gnome-terminal -x bash -c "(./hyperchain -o $j -l 808$j -p $CONFIG_PATH)"
+  done
 
-   echo "All process are running background"
+  echo "All process are running background"
 
 }
 ########################
 #   server side test   #
 ########################
 server_test(){
-if [ ! -f "/usr/bin/expect" ];then
- echo "hasn't install expect,please install expect mannualy: 'apt-get install expect'"
- exit 1
-fi
+#if [ ! -f "/usr/bin/expect" ];then
+# echo "hasn't install expect,please install expect mannualy: 'apt-get install expect'"
+# exit 1
+#fi
 #SERVER_ADDR=( cn6.hyperchain.cn cn7.hyperchain.cn cn8.hyperchain.cn cn9.hyperchain.cn cn12.hyperchain.cn cn13.hyperchain.cn cn14.hyperchain.cn)
 # get the server list config
 while read line;do
-  SERVER_ADDR+=" ${line}"
+ SERVER_ADDR+=" ${line}"
 done < ./serverlist.txt
 
- ni=1
- for server_address in ${SERVER_ADDR[@]}; do
-   i=`expr $ni + 1`
-   # gnome-terminal -x bash -c "ssh satoshi@$server_address \"cd /home/satoshi/gopath/src/hyperchain/scripts/ && ./server.sh $i\""
-   if [ $ni -eq 1 ];then
-       gnome-terminal -x bash -c "ssh satoshi@$server_address \"cd /home/satoshi/gopath/src/hyperchain/scripts/ && ./server.sh $ni\""
-   else
-       nohup ssh satoshi@$server_address "source /home/satoshi/.profile && cd /home/satoshi/gopath/src/hyperchain/scripts/ && ./server.sh $ni" &
-   fi
- done
+ni=1
+for server_address in ${SERVER_ADDR[@]}; do
+  i=`expr $ni + 1`
+  # gnome-terminal -x bash -c "ssh satoshi@$server_address \"cd /home/satoshi/gopath/src/hyperchain/scripts/ && ./server.sh $i\""
+  ssh satoshi@$server_address "chmod a+x /home/satoshi/gopath/src/hyperchain/scripts/server.sh && bash /home/satoshi/gopath/src/hyperchain/scripts/server.sh $ni"
+done
 }
 
 #set -x
 (( $# != 1 )) && { echo -e >&2 "Usage: $0 1 : 本地测试, $0 2 : 服务器测试"; exit 1; }
 CONFIG_PATH="./p2p/local_peerconfig.json"
 if [ $1 == 1 ];then
-   echo -e "开启本地测试"
-   local_test "./p2p/local_peerconfig.json"
+  echo -e "开启本地测试"
+  local_test "./p2p/local_peerconfig.json"
 elif [ $1 == 2 ];then
-   echo -e "开启服务器测试"
-   server_test
+  echo -e "开启服务器测试"
+  server_test
 elif [ $1 == 3 ];then
-   echo -e "杀死所有进程"
-   kellprogress
-   exit
+  echo -e "杀死所有进程"
+  kellprogress
+  exit
 else
-   echo "参数错误"
-   exit 1
+  echo "参数错误"
+  exit 1
 fi
