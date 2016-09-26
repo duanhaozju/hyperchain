74ba659c758fbf94107f5bb261d3d5b5a88bae37
meilinghu <meiling.hu@hyperchain.cn>
2016-09-26 17:04:01 +0800

    add BlockAvgTime calculation

--- jsonrpc/Dashboard/static/js/services.js
+++ jsonrpc/Dashboard/static/js/services.js
@@ -161,6 +161,32 @@ function BlockService($resource,$q,ENV) {
 
                 })
             })
+        },
+        queryBlockAvgTime: function(from, to) {
+            return $q(function(resolve, reject){
+                $resource(ENV.API,{},{
+                    query:{
+                        method:"POST"
+                    }
+                }).query({
+                    method: "block_queryBlockAvgTime",
+                    params: [
+                        {
+                            "from":from,
+                            "to":to
+                        }
+                    ],
+                    id: 1
+                },function(res){
+                    console.log(res);
+                    if (res.error) {
+                        reject(res.error)
+                    } else {
+                        resolve(res.result)
+                    }
+
+                })
+            })
         }
     }
 }
