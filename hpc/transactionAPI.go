95f6d9482f570b249280f1f135f5a113350b9205
sammy <mamxxim@163.com>
2016-10-10 18:55:21 +0800

    merge branch EVM

--- hpc/transactionAPI.go
+++ hpc/transactionAPI.go
@@ -201,7 +201,7 @@ func (tran *PublicTransactionAPI) SendTransaction(args SendTxArgs) (common.Hash,
 		//go manager.GetEventObject().Post(event.NewTxEvent{Payload: txBytes})
 		log.Infof("############# %d: start send request#############", time.Now().Unix())
 		start := time.Now().Unix()
-		end := start + 30
+		end := start + 6
 		//end:=start+500
 
 		for start := start; start < end; start = time.Now().Unix() {
