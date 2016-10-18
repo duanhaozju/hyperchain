7b060e14a1bb9cc8d735aefae1812d33c01ae9ca
chenquan <quan.chen@hyperchain.cn>
2016-10-18 18:38:52 +0800

    build the frame work

--- scripts/genconfig.py
+++ scripts/genconfig.py
@@ -18,21 +18,47 @@ import yaml
 #
 # 请加上 default_flow_style=False
 # print yaml.dump(yaml.load(document), default_flow_style=False)
-class PeerConfig:
-    def _init_(self):
-        self.nodeID = 1
-        self.localGRPCPort=8001
-        self.localJsonRPCPort=8081
-        self.localIP="127.0.0.1"
-    def genPeerConfig(self,node_ID=1,local_gRPC_Port=8001,local_JsonRPC_Port=8081):
-        pass
-    def readServerList(self,server_list_path):
-        server_list_file = open(server_list_path,"rb")
-        for line in server_list_file.readlines():
-            print line.strip()
-        server_list_file.close()
 
-        
-if __name__ == '__main__':
-    peerConfig = PeerConfig()
-    peerConfig.readServerList("server.txt")
+# global config
+# this section should give some params
+
+glb = {"global":{}}
+nodeid = {'nodeid':1}
+grpcport = {'grpcprort':8001}
+httpport = {'httpport':8081}
+
+glb['global'].update(nodeid)
+glb['global'].update(grpcport)
+glb['global'].update(httpport)
+#print glb
+
+
+# data storage config
+# this section should not modified!
+account = {'keystoredir':'"config/keystore"','keynodesdir':'"./build/keynodes"'}
+
+logs = {'logs':{'dumpfile':True,'logsdir':'"./build/logs"','logllevel':'NOTICE'}}
+
+database = {'database':{'dir':'"./build/database"'}}
+glb['global'].update(account)
+glb['global'].update(logs)
+glb['global'].update(database)
+
+#print glb
+# configs section, this section should use default configs
+configs = {'configs':{}}
+peers = {'peers':'"configs/peerconfig.json"'}
+genesis = {'genesis':'"config/genesis.json"'}
+memebersrvc = {'membersrvc':'"config/membersrvc.yaml"'}
+pbft = {'pbft':'"config/pbft.yaml"'}
+configs['configs'].update(peers)
+configs['configs'].update(genesis)
+configs['configs'].update(memebersrvc)
+configs['configs'].update(pbft)
+
+glb["global"].update(configs)
+#print glb
+
+
+#print yaml.dump(yaml.load(glb),default_flow_style=False)
+print yaml.dump(glb,default_flow_style=False)
