9691890668e5c1cb7fe6217bc7b3bf8bb6f2d295
sylvia@ubuntu_fox <fanglei.huang@hyperchain.cn>
2016-12-16 17:05:52 +0800

    delete the test code

--- main.go
+++ main.go
@@ -129,15 +129,6 @@ func main() {
 		if blockPool == nil {
 			return errors.New("Initialize BlockPool failed")
 		}
-		// JUST FOR TEST
-		// MODIFY LATEST BLOCK HASH WHEN RECONNECT
-		if argv.IsReconnect == true && argv.NodeID == 4 {
-			db, _ := hyperdb.GetLDBDatabase()
-			latest, _ := core.GetBlockByNumber(db, core.GetChainCopy().Height)
-			latest.BlockHash = []byte("fakeBlockHash")
-			core.PersistBlock(db.NewBatch(), latest, config.getBlockVersion(), true, true)
-			core.UpdateChainByBlcokNum(db, core.GetChainCopy().Height)
-		}
 
 		//init manager
 		exist := make(chan bool)
