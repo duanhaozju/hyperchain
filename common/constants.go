112630d00a58f2f8b06dd82621fee121a9566510
Xiaoyi Wang <wangxiaoyi@hyperchain.cn>
2017-01-05 10:35:09 +0800

    Make log file divisible by interval

--- common/constants.go
+++ common/constants.go
@@ -8,8 +8,9 @@ const (
 	LOG_FILE_DIR  = "global.logs.logsdir"
 	LOG_FUMP_FILE  = "global.logs.dumpfile"
 	LOG_BASE_LOG_LEVEL = "global.logs.loglevel"
+	LOG_NEW_FILE_INTERVAL = "global.logs.newLogFileInterval"
 
-	LOG_MODUKE_KEY = "global.logs.module"
+	LOG_MODULE_KEY = "global.logs.module"
 )
 
 
