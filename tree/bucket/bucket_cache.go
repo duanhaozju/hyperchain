db563da73bd3651ec68b7b2c8267797f5f28bb58
HaizhenZhuo <zhuohaizhen@gmail.com>
2017-01-12 11:45:41 +0800

    update the global datanodecache

--- tree/bucket/bucket_cache.go
+++ tree/bucket/bucket_cache.go
@@ -7,7 +7,7 @@ import (
 )
 
 var defaultBucketCacheMaxSize = 100 // MBs
-var defaultDataNodeCacheMaxSize = 100 // MBs
+var defaultDataNodeCacheMaxSize = 10000
 
 // We can create a cache and keep all the bucket nodes pre-loaded.
 // Since, the bucket nodes do not contain actual data and max possible
