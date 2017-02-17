43078926b2385004d1561d1a2377692262370a13
Xiaoyi Wang <wangxiaoyi@hyperchain.cn>
2017-02-17 17:54:06 +0800

    persist the bloom filter

--- hyperdb/key_index.go
+++ hyperdb/key_index.go
@@ -1,82 +1,163 @@
-////Hyperchain License
-////Copyright (C) 2016 The Hyperchain Authors.
+//Hyperchain License
+//Copyright (C) 2016 The Hyperchain Authors.
 package hyperdb
-//
-//import (
-//	"github.com/willf/bloom"
-//	"github.com/syndtr/goleveldb/leveldb"
-//)
-//
-////IndexManager manage indexes by the namespace
-//type IndexManager struct {
-//	indexs map[string] Index
-//}
-//
-//type Index interface {
-//	Namespace() string
-//	Contains(key interface{}) bool
-//	MayContains(key interface{}) bool
-//	GetIndex(key interface{}) interface{}
-//	AddIndexForKey(key interface{}) error
-//	Init() error
-//	Rebuild() error
-//}
-//
-////index implement for key-value store
-////based on BloomFilter
-//type KeyIndex struct {
-//	namespace string
-//	bf        *bloom.BloomFilter
-//	db        *leveldb.DB
-//}
-//
-////Namespace get namespace of this keyindex instance
-//func (ki KeyIndex) Namespace() string  {
-//	return ki.namespace
-//}
-//
-////Contains judge whether the key is stored in the underlying database
-//func (ki KeyIndex) Contains(key []byte) bool {
-//	//TODO: Contains(key []byte)
-//	return false
-//}
-//
-////MayContains if return true then the key may stored in the underlying database
-////of return false, the key must not stored in the underlying database
-//func (ki KeyIndex) MayContains(key []byte) bool {
-//	return ki.bf.Test(key)
-//}
-//
-////GetIndex get the index of the specify key
-//func (ki KeyIndex) GetIndex(key interface{}) interface {} {
-//	log.Debugf("Get index for key: %v", key)
-//	return nil
-//}
-//
-////Init the index
-//func (ki KeyIndex) Init() error  {
-//	log.Debug("Init KeyIndex")
-//	return nil
-//}
-//
-////Rebuild rebuild the index
-//func (ki KeyIndex) Rebuild() error {
-//
-//
-//	return nil
-//}
-//
-////NewKeyIndex new KeyIndex instance
-//func NewKeyIndex(ns string, db *leveldb.DB) Index {
-//	filter := bloom.New(10000 * 10000, 3)
-//	return &KeyIndex{
-//		namespace:ns,
-//		bf:filter,
-//		db:db,
-//	}
-//}
-//
-//func (ki KeyIndex) AddIndexForKey(key interface{}) error {
-//	ki.bf.Add(key)
-//	return nil
-//}
+
+import (
+	"github.com/willf/bloom"
+	"github.com/syndtr/goleveldb/leveldb"
+	"time"
+	"github.com/syndtr/goleveldb/leveldb/util"
+	"os"
+	//"bytes"
+)
+
+//IndexManager manage indexes by the namespace.
+type IndexManager struct {
+	indexs map[string] Index
+}
+
+var bloomPath = "bloom.dat"
+
+//GetIndex get index by the namespace.
+func (im IndexManager) GetIndex(ns string) Index {
+	if index, ok := im.indexs[ns]; ok {
+		return index
+	}
+	return nil
+}
+
+//AddIndex add index if not existed.
+func (im IndexManager) AddIndex(i Index)  {
+	if _, ok := im.indexs[i.Namespace()]; ok {
+		log.Warningf("index existed: %v", i)
+	}
+	im.indexs[i.Namespace()] = i
+}
+
+//Index interface of the data index.
+type Index interface {
+	Namespace() string
+	Contains(key []byte) bool
+	MayContains(key []byte) bool
+	GetIndex(key []byte) interface{}
+	AddIndexForKey(key []byte) error
+	Init() error
+	Rebuild() error
+}
+
+//KeyIndex index implement for key-value store based on BloomFilter.
+type KeyIndex struct {
+	namespace          string
+	bf                 *bloom.BloomFilter
+	db                 *leveldb.DB
+	keyPrefix          []byte
+	lastStartKeyPrefix []byte
+	currStartKeyPrefix []byte
+}
+
+//Namespace get namespace of this keyindex instance
+func (ki KeyIndex) Namespace() string  {
+	return ki.namespace
+}
+
+//Contains judge whether the key is stored in the underlying database
+func (ki KeyIndex) Contains(key []byte) bool {
+	//TODO: Contains(key []byte)
+	return true
+}
+
+//MayContains if return true then the key may stored in the underlying database
+//of return false, the key must not stored in the underlying database
+func (ki KeyIndex) MayContains(key []byte) bool {
+	return ki.bf.Test(key)
+}
+
+//GetIndex get the index of the specify key
+func (ki KeyIndex) GetIndex(key []byte) interface {} {
+	log.Debugf("Get index for key: %v", key)
+	return nil
+}
+
+func (ki KeyIndex) lastStartKey() []byte  {
+	return []byte(ki.namespace + "_start_key")
+}
+
+//Init the index
+func (ki KeyIndex) Init() error  {
+	log.Debug("Init KeyIndex")
+	var err error
+	ki.lastStartKeyPrefix, err =  ki.db.Get(ki.lastStartKey(), nil)
+	if err != nil {
+		return err
+	}
+	log.Debugf("Last start key index: %v", ki.lastStartKeyPrefix)
+	ki.currStartKeyPrefix = []byte(string(ki.keyPrefix) + string(time.Now().UnixNano()) + ".")
+	ki.keyPrefix = ki.currStartKeyPrefix
+	log.Debugf("Current start key index: %v", ki.currStartKeyPrefix)
+	if ok, err := ki.db.Has(ki.lastStartKeyPrefix, nil); ok && err == nil {
+		ki.Rebuild()
+	}
+
+	ki.db.Put(ki.lastStartKey(), ki.currStartKeyPrefix, nil)
+	return err
+}
+
+//Rebuild rebuild the index
+func (ki KeyIndex) Rebuild() error {
+	it := ki.db.NewIterator(util.BytesPrefix(ki.lastStartKeyPrefix), nil)
+	for ;it.Next(); {
+		key := it.Key()
+		ki.bf.Add(key)
+	}
+	return nil
+}
+
+//NewKeyIndex new KeyIndex instance
+func NewKeyIndex(ns string, db *leveldb.DB) Index {
+	filter := bloom.New(10000 * 10000, 3)
+	return &KeyIndex{
+		namespace:ns,
+		bf:filter,
+		db:db,
+		keyPrefix:[]byte(ns + "_bloom_key."),
+	}
+}
+
+//AddIndexForKey add index for a specify key
+func (ki KeyIndex) AddIndexForKey(key []byte) error {
+	ki.bf.Add(key)
+	//ki.persistKey(key)
+	return nil
+}
+
+//persist the key into the database
+func (ki KeyIndex) persistKey(key []byte) error {
+	//key = append(ki.keyPrefix, key)
+
+	key = []byte(string(ki.keyPrefix) + string(key))
+	return ki.db.Put(key, []byte(time.Now().String()), nil)
+}
+
+func (ki KeyIndex) dropPreviousKey() error  {
+	it := ki.db.NewIterator(util.BytesPrefix(ki.lastStartKeyPrefix), nil)
+	for ;it.Next(); {
+		key := it.Key()
+		ki.db.Delete(key, nil)
+	}
+	return nil
+}
+
+//persistBloom persist bloom filter
+func (ki KeyIndex) persistBloom () error {
+	inputFile, err := os.Open(bloomPath)
+	if err == nil {
+		ki.bf.WriteTo(inputFile)
+	}else {
+		inputFile, err = os.Create(bloomPath)
+		if err != nil {
+			ki.bf.WriteTo(inputFile)
+		}
+
+	}
+	return err
+}
--- hyperdb/super_level_db.go
+++ hyperdb/super_level_db.go
@@ -8,66 +8,49 @@ import (
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/iterator"
 	"github.com/syndtr/goleveldb/leveldb/util"
-	"github.com/willf/bloom"
 )
 
 type SuperLevelDB struct {
 	path  string
 	db    *leveldb.DB
-	bf    *bloom.BloomFilter //TODO: we may need index for keys stored in the db
-	//cache *cache.Cache
+	index Index
 }
 
 //New new super LevelDB
 func NewSLDB(filepath string) (*SuperLevelDB, error) {
 	db, err := leveldb.OpenFile(filepath, nil)
-	filter := bloom.New(10000 * 10000, 3) //TODO: make this configurable
-
-	//c := cache.New(5*time.Minute, 30 * time.Second)
+	index := NewKeyIndex("defaultNS", db)
 
 	return &SuperLevelDB{
 		path: filepath,
 		db:   db,
-		bf:   filter,
-		//cache:c,
+		index:   index,
 	}, err
 }
-//var y int = 0
+
+//Put put key value data into the database.
 func (sldb  *SuperLevelDB) Put(key []byte, value []byte) error {
-	//t1 := time.Now()
 	err := sldb.db.Put(key, value, nil)
 	if err == nil {
-		sldb.bf.Add(key)
-		//sldb.cache.Add(string(key), value, 5 * time.Minute)
+		sldb.index.AddIndexForKey(key)
 	}
-	//y ++
-	//log.Warningf("%d put used %v", y, time.Now().Sub(t1))
 	return err
 }
 
-var getCount int = 0
+//Get fetch data for specify key from db.
 func (sldb *SuperLevelDB) Get(key []byte) ([]byte, error) {
 	var data []byte
 	var err error
-	//t1 := time.Now()
-	if sldb.bf.Test(key) {
-		//if val, ok := sldb.cache.Get(string(key)); ok {
-		//	if dat, ok := (val).([]byte); ok {
-		//		return dat, nil
-		//	}
-		//}
+	if sldb.index.MayContains(key) {
 		data, err = sldb.db.Get(key, nil)
 	}else {
 		err = leveldb.ErrNotFound
 	}
-	//getCount ++
-	//log.Warningf("%d get used %v", getCount, time.Now().Sub(t1))
 	return data, err
 }
 
-// Delete deletes the value for the given key
+// Delete deletes the value for the given key.
 func (sldb *SuperLevelDB) Delete(key []byte) error {
-	//sldb.cache.Delete(string(key))
 	return sldb.db.Delete(key, nil)
 }
 
@@ -81,7 +64,7 @@ func (sldb *SuperLevelDB) NewIteratorWithPrefix(prefix []byte) iterator.Iterator
 }
 
 //Destroy, clean the whole database,
-//warning: bad performance if to many data in the db
+//warning: bad performance if too many data in the db
 func (sldb *SuperLevelDB) Destroy() error {
 	return sldb.DestroyByRange(nil, nil)
 }
@@ -105,53 +88,31 @@ func (sldb *SuperLevelDB) Close() {
 	sldb.db.Close()
 }
 
-func (sldb *SuperLevelDB) LDB() *leveldb.DB {
-	return sldb.db
-}
-
 func (db *SuperLevelDB) NewBatch() Batch {
-	log.Debugf("new super level db batch")
-
+	log.Debugf("new super leveldb batch")
 	return &superLdbBatch{
 		batch: new(leveldb.Batch),
 		sldb:db,
-		//batchMap:make(map[string]interface{}),
 		}
 }
 
 type superLdbBatch struct {
 	batch *leveldb.Batch
 	sldb  *SuperLevelDB
-	//batchMap    map[string]interface{}
-	//bl sync.Mutex
 }
 
-func (b *superLdbBatch) Put(key, value []byte) error {
-	b.batch.Put(key, value)
-	b.sldb.bf.Add(key)
-	//b.bl.Lock()
-	//defer b.bl.Unlock()
-	//value1 := make([]byte, len(value))
-	//copy(value1, value)
-	//b.batchMap[string(key)]=value1
+func (sb *superLdbBatch) Put(key, value []byte) error {
+	sb.batch.Put(key, value)
+	sb.sldb.index.AddIndexForKey(key)
 	return nil
 }
 
 func (b *superLdbBatch) Delete(key []byte) error {
 	b.batch.Delete(key)
-	//b.sldb.cache.Delete(string(key))
 	return nil
 }
-//var x int = 0
+
 func (b *superLdbBatch) Write() error {
-	//b.bl.Lock()
-	//for k, v := range b.batchMap{
-	//	b.sldb.cache.Add(k, v, 5 * time.Minute)
-	//}
-	//b.bl.Unlock()
-	//x ++
-	//t1 := time.Now()
 	err := b.sldb.db.Write(b.batch, nil)
-	//log.Warningf("%d batch write time used: %v", x,  time.Now().Sub(t1))
 	return err
 }
