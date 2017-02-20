0e42413ba8b0df4b6a2628eb12bce9daa3bafaa8
Sylvia@mac_panda <hfanglei@gmail.com>
2017-02-20 18:36:16 +0800

    Bug #233 bug in recovery done we should call executeAfterStateUpdate (while lastExec equal to recoveryToSeqNo)

--- consensus/pbft/pbftprotocal.go
+++ consensus/pbft/pbftprotocal.go
@@ -1077,6 +1077,7 @@ func (pbft *pbftProtocal) recvStateUpdatedEvent(et *stateUpdatedEvent) error {
 			pbft.recoveryToSeqNo = nil
 			pbft.recoveryRestartTimer.Stop()
 			go pbft.postPbftEvent(recoveryDoneEvent{})
+			pbft.executeAfterStateUpdate()
 			return nil
 		}
 
@@ -1170,7 +1171,7 @@ func (pbft *pbftProtocal) preValidate(idx msgID) bool {
 	}
 
 	if idx.n != pbft.lastVid+1 {
-		logger.Debugf("Backup %d hasn't done with last validate %d", pbft.id, pbft.lastVid)
+		logger.Debugf("Backup %d gets validateBatch seqNo=%d, but expect seqNo=%d", pbft.id, idx.n, pbft.lastVid+1)
 		return false
 	}
 
