29096bac1416ffefa4877c4e1170ad12ea196f22
Xiaoyi Wang <wangxiaoyi@hyperchain.cn>
2017-03-13 18:41:16 +0800

    Implement init logic for namespace instance

--- hyperdb/api.go
+++ hyperdb/api.go
@@ -7,40 +7,39 @@ import (
 	"fmt"
 	"github.com/op/go-logging"
 	"github.com/spf13/viper"
-	"strconv"
-	"sync"
-	"hyperchain/hyperdb/sldb"
 	"hyperchain/common"
 	"hyperchain/hyperdb/db"
 	"hyperchain/hyperdb/hleveldb"
+	"hyperchain/hyperdb/sldb"
+	"strconv"
+	"sync"
 )
+
 //TODO: refactor this file as soon as possible
 var (
 	logPath   = ""
 	logStatus = false
-	dbType = 001
-	grpcPort             = 8001
-	leveldbPath          = "./build/leveldb"
+	dbType    = 001
 )
 
 type stateDb int32
 
 type DBInstance struct {
-	db     db.Database
-	state  stateDb
+	db    db.Database
+	state stateDb
 }
 
 const (
 	DB_TYPE = "dbConfig.dbType"
 
 	// database type
-	LDB_DB = 0001
+	LDB_DB         = 0001
 	SUPER_LEVEL_DB = 0010
 
 	// namespace
-	DefautNameSpace="Global"
-	Blockchain="Blockchain"
-	Consensus="Consensus"
+	DefautNameSpace = "global"
+	Blockchain      = "Blockchain"
+	Consensus       = "Consensus"
 
 	// state
 	closed stateDb = iota
@@ -48,17 +47,17 @@ const (
 )
 
 var log *logging.Logger // package-level logger
-type DbMap struct{
-	dbMap map[string] *DBInstance
-	dbSync  sync.Mutex
+type DbMap struct {
+	dbMap  map[string]*DBInstance
+	dbSync sync.Mutex
 }
 
-var  dbMap *DbMap
+var dbMap *DbMap
 
 func init() {
 	log = logging.MustGetLogger("hyperdb")
-	dbMap=&DbMap{
-		dbMap:make(map[string] *DBInstance),
+	dbMap = &DbMap{
+		dbMap: make(map[string]*DBInstance),
 	}
 }
 
@@ -75,9 +74,6 @@ func SetDBConfig(dbConfig string, port string) {
 	logStatus = config.GetBool("dbConfig.logStatus")
 	logPath = config.GetString("dbConfig.logPath")
 
-	leveldbPath = config.GetString("dbConfig.leveldbPath")
-	grpcPort, _ = strconv.Atoi(port)
-
 }
 
 func IfLogStatus() bool {
@@ -89,26 +85,25 @@ func GetLogPath() string {
 }
 
 func InitDatabase(conf *common.Config, nameSpace string) error {
-
+	log.Criticalf("namespace %s", nameSpace)
 	dbMap.dbSync.Lock()
 	defer dbMap.dbSync.Unlock()
-	_,ok:=dbMap.dbMap[nameSpace+Blockchain]
+	_, ok := dbMap.dbMap[nameSpace+Blockchain]
 
-	if ok{
-		log.Notice("Try to init inited db "+nameSpace)
-		return errors.New("Try to init inited db "+nameSpace)
+	if ok {
+		log.Notice("Try to init inited db " + nameSpace)
+		return errors.New("Try to init inited db " + nameSpace)
 	}
 
 	db, err := NewDatabase(conf, "Blockchain", dbType)
 
-
-	if err!=nil{
+	if err != nil {
 		log.Errorf(fmt.Sprintf("InitDatabase(%v) fail beacause it can't get new database \n", nameSpace))
 		log.Error(err.Error())
 		return errors.New(fmt.Sprintf("InitDatabase(%v) fail beacause it can't get new database \n", nameSpace))
 	}
 
-	db1, err1 := NewDatabase(conf, "Consensus" , 001)
+	db1, err1 := NewDatabase(conf, "Consensus", 001)
 
 	if err1 != nil {
 
@@ -116,14 +111,14 @@ func InitDatabase(conf *common.Config, nameSpace string) error {
 		return errors.New(fmt.Sprintf("InitDatabase(%v) fail beacause it can't get new database \n", nameSpace))
 	}
 
-	dbMap.dbMap[nameSpace+Blockchain]=&DBInstance{
+	dbMap.dbMap[nameSpace+Blockchain] = &DBInstance{
 		state: opened,
-		db:db,
+		db:    db,
 	}
 
-	dbMap.dbMap[nameSpace+Consensus]=&DBInstance{
+	dbMap.dbMap[nameSpace+Consensus] = &DBInstance{
 		state: opened,
-		db:db1,
+		db:    db1,
 	}
 
 	return err
@@ -139,48 +134,38 @@ func GetDBDatabase() (db.Database, error) {
 	return dbMap.dbMap[DefautNameSpace+Blockchain].db, nil
 }
 
-func GetDBDatabaseConsensus() (db.Database, error) {
-	dbMap.dbSync.Lock()
-	defer dbMap.dbSync.Unlock()
-	if dbMap.dbMap[DefautNameSpace+Consensus].db == nil {
-		log.Notice("GetDBDatabaseConsensus()  fail beacause dbMap[GlobalConsensus] has not been inited \n")
-		return nil, errors.New("GetDBDatabaseConsensus()  fail beacause dbMap[GlobalConsensus] has not been inited \n")
-	}
-	return dbMap.dbMap[DefautNameSpace+Consensus].db, nil
-}
-
-func GetDBDatabaseByNamespace(namespace string)(db.Database, error){
+func GetDBDatabaseByNamespace(namespace string) (db.Database, error) {
 	dbMap.dbSync.Lock()
 	defer dbMap.dbSync.Unlock()
 
 	namespace += Blockchain
 
-	if _,ok:=dbMap.dbMap[namespace];!ok{
-		log.Notice(fmt.Sprintf("GetDBDatabaseByNamespcae fail beacause dbMap[%v] has not been inited \n",namespace))
-		return nil, errors.New(fmt.Sprintf("GetDBDatabaseByNamespcae fail beacause dbMap[%v] has not been inited \n",namespace))
+	if _, ok := dbMap.dbMap[namespace]; !ok {
+		log.Notice(fmt.Sprintf("GetDBDatabaseByNamespcae fail beacause dbMap[%v] has not been inited \n", namespace))
+		return nil, errors.New(fmt.Sprintf("GetDBDatabaseByNamespcae fail beacause dbMap[%v] has not been inited \n", namespace))
 	}
 
 	if dbMap.dbMap[namespace].db == nil {
-		log.Notice(fmt.Sprintf("GetDBDatabaseByNamespcae fail beacause dbMap[%v] has not been inited \n",namespace))
-		return nil, errors.New(fmt.Sprintf("GetDBDatabaseByNamespcae fail beacause dbMap[%v] has not been inited \n",namespace))
+		log.Notice(fmt.Sprintf("GetDBDatabaseByNamespcae fail beacause dbMap[%v] has not been inited \n", namespace))
+		return nil, errors.New(fmt.Sprintf("GetDBDatabaseByNamespcae fail beacause dbMap[%v] has not been inited \n", namespace))
 	}
 	return dbMap.dbMap[namespace].db, nil
 }
 
-func GetDBConsensusByNamespcae(namespace string)(db.Database, error){
+func GetDBConsensusByNamespcae(namespace string) (db.Database, error) {
 	dbMap.dbSync.Lock()
 	defer dbMap.dbSync.Unlock()
 
-	namespace+=Consensus
+	namespace += Consensus
 
-	if _,ok:=dbMap.dbMap[namespace];!ok{
-		log.Notice(fmt.Sprintf("GetDBConsensusByNamespcae fail beacause dbMap[%v] has not been inited \n",namespace))
-		return nil, errors.New(fmt.Sprintf("GetDBConsensusByNamespcae fail beacause dbMap[%v] has not been inited \n",namespace))
+	if _, ok := dbMap.dbMap[namespace]; !ok {
+		log.Notice(fmt.Sprintf("GetDBConsensusByNamespcae fail beacause dbMap[%v] has not been inited \n", namespace))
+		return nil, errors.New(fmt.Sprintf("GetDBConsensusByNamespcae fail beacause dbMap[%v] has not been inited \n", namespace))
 	}
 
 	if dbMap.dbMap[namespace].db == nil {
-		log.Notice(fmt.Sprintf("GetDBConsensusByNamespcae fail beacause dbMap[%v] has not been inited \n",namespace))
-		return nil, errors.New(fmt.Sprintf("GetDBConsensusByNamespcae fail beacause dbMap[%v] has not been inited \n",namespace))
+		log.Notice(fmt.Sprintf("GetDBConsensusByNamespcae fail beacause dbMap[%v] has not been inited \n", namespace))
+		return nil, errors.New(fmt.Sprintf("GetDBConsensusByNamespcae fail beacause dbMap[%v] has not been inited \n", namespace))
 	}
 	return dbMap.dbMap[namespace].db, nil
 }
@@ -189,10 +174,10 @@ func NewDatabase(conf *common.Config, path string, dbType int) (db.Database, err
 	switch dbType {
 	case LDB_DB:
 		log.Notice("Use level db only")
-		return hleveldb.NewLDBDataBase(conf,path)
+		return hleveldb.NewLDBDataBase(conf, path)
 	case SUPER_LEVEL_DB:
 		log.Notice("Use SuperLevelDB")
-		return sldb.NewSLDB(conf,path)
+		return sldb.NewSLDB(conf, path)
 	default:
 		log.Errorf("Wrong dbType:" + strconv.Itoa(dbType))
 		return nil, errors.New("Wrong dbType:" + strconv.Itoa(dbType))
--- namespace/common.go
+++ namespace/common.go
@@ -4,15 +4,37 @@ package namespace
 
 import (
 	"hyperchain/common"
-	"os"
+	"github.com/spf13/viper"
 )
 
 const (
-	NAMESPACE_CONFIG_DIR_ROOT = "gloable.nsConfigRootPath"
+	NS_CONFIG_DIR_ROOT = "global.nsConfigRootPath"
+	NS_CONFIG_DIR      = "namespace.config.dir"
 )
 
 //ConstructConfigFromDir read all info needed by
-func ConstructConfigFromDir(dir os.FileInfo) *common.Config {
-	//TODO: read configuration for a single namespace
-	return nil
+func ConstructConfigFromDir(path string) *common.Config {
+	nsConfigPath := path + "/global.yaml"
+	conf := common.NewConfig(nsConfigPath)
+	// init peer configurations
+	peerConfigPath := conf.GetString("global.configs.peers")
+	peerViper := viper.New()
+	peerViper.SetConfigFile(peerConfigPath)
+	err := peerViper.ReadInConfig()
+	if err != nil {
+		logger.Errorf("err %v", err)
+	}
+	nodeID := peerViper.GetInt("self.node_id")
+	grpcPort := peerViper.GetInt("self.grpc_port")
+	jsonrpcPort := peerViper.GetInt("self.jsonrpc_port")
+	restfulPort := peerViper.GetInt("self.restful_port")
+
+	conf.Set(common.C_NODE_ID, nodeID)
+	conf.Set(common.C_HTTP_PORT, jsonrpcPort)
+	conf.Set(common.C_REST_PORT, restfulPort)
+	conf.Set(common.C_GRPC_PORT, grpcPort)
+	conf.Set(common.C_PEER_CONFIG_PATH, peerConfigPath)
+	conf.Set(common.C_GLOBAL_CONFIG_PATH, nsConfigPath)
+
+	return conf
 }
