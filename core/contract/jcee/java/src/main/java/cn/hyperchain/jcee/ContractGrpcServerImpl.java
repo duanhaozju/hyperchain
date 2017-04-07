16e66460f7d8c65d6df5da4092b20774f029a52e
Xiaoyi Wang <wangxiaoyi@hyperchain.cn>
2017-04-07 16:43:08 +0800

    Impl java smart contract executor framwork

--- core/contract/jcee/java/src/main/java/cn/hyperchain/jcee/ContractGrpcServerImpl.java
+++ core/contract/jcee/java/src/main/java/cn/hyperchain/jcee/ContractGrpcServerImpl.java
@@ -4,19 +4,22 @@
  */
 package cn.hyperchain.jcee;
 
-import cn.hyperchain.jcee.contract.ContractManager;
-import cn.hyperchain.jcee.executor.ContractExecutor;
 import cn.hyperchain.protos.ContractGrpc;
 import cn.hyperchain.protos.Request;
 import cn.hyperchain.protos.Response;
 import io.grpc.stub.StreamObserver;
+import org.apache.log4j.Logger;
 
 public class ContractGrpcServerImpl extends ContractGrpc.ContractImplBase {
-    private ContractManager cm;
-    private ContractExecutor executor;
+    private final String query = "query";
+    private final String invoke = "invoke";
+    private final String deploy = "deploy";
+
+    private static final Logger logger = Logger.getLogger(ContractGrpcServerImpl.class.getSimpleName());
+    private Handler handler;
 
     public ContractGrpcServerImpl() {
-
+        handler = new Handler();
     }
     /**
      * @param request
@@ -24,9 +27,21 @@ public class ContractGrpcServerImpl extends ContractGrpc.ContractImplBase {
      */
     @Override
     public void execute(Request request, StreamObserver<Response> responseObserver) {
-        Response r = Response.newBuilder().setOk(true).build();
-        responseObserver.onNext(r);
-        responseObserver.onCompleted();
+        switch (request.getMethod()) {
+            case query:{
+                handler.query(request, responseObserver);
+                break;
+            }
+            case invoke: {
+                handler.invoke(request, responseObserver);
+                break;
+            }
+            case deploy: {
+                handler.deploy(request, responseObserver);
+            }
+            default:
+                logger.error("method " + request.getMethod() + " is not implemented yet!");
+        }
     }
 
     /**
@@ -38,5 +53,13 @@ public class ContractGrpcServerImpl extends ContractGrpc.ContractImplBase {
         Response r = Response.newBuilder().setOk(true).build();
         responseObserver.onNext(r);
         responseObserver.onCompleted();
+    }
+
+    public Handler getHandler() {
+        return handler;
+    }
+
+    public void setHandler(Handler handler) {
+        this.handler = handler;
     }
 }
