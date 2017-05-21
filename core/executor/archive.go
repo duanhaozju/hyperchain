b1e9397c6712e01b7d0b96a88cc9800f85d6ebc2
rjl493456442 <garyrong0905@gmail.com>
2017-05-21 15:55:39 +0800

    feat: add a snapshot registry for maintain sq request

--- core/executor/archive.go
+++ core/executor/archive.go
@@ -3,25 +3,58 @@ package executor
 import (
 	"hyperchain/manager/event"
 	edb "hyperchain/core/db_utils"
+	"github.com/op/go-logging"
+	"sync"
 )
 
 const LatestBlockNumber uint64 = 0
 
-func (executor *Executor) Snapshot(event event.SnapshotEvent) {
-	if !executor.checkSnapshotReq(event) {
+
+type SnapshotRegistry struct {
+	namespace  string
+	rq         map[uint]struct{}
+	rqLock     sync.RWMutex
+	logger     *logging.Logger
+}
+
+func NewSnapshotRegistry(namespace string, logger *logging.Logger) *SnapshotRegistry {
+	return &SnapshotRegistry{
+		namespace:  namespace,
+		rq:         make(map[uint]struct{}),
+		logger:     logger,
+	}
+}
+
+
+func (registry *SnapshotRegistry) addRequest(event event.SnapshotEvent) {
+	registry.rqLock.Lock()
+	defer registry.rqLock.Unlock()
+	registry.rq[event.BlockNumber] = struct{}{}
+}
+
+
+func (registry *SnapshotRegistry) Snapshot(event event.SnapshotEvent) {
+	if !registry.checkSnapshotReq(event) {
 		// TODO make a notification event
 		// TODO wait @Sammy finish event system
+	} else {
+		registry.addRequest(event)
 	}
 
 }
 
-func (executor *Executor) checkSnapshotReq(event event.SnapshotEvent) bool {
+func (registry *SnapshotRegistry) checkSnapshotReq(event event.SnapshotEvent) bool {
 	if event.BlockNumber == LatestBlockNumber {
 		return true
 	}
-	chainHeight := edb.GetHeightOfChain(executor.namespace)
+	chainHeight := edb.GetHeightOfChain(registry.namespace)
 	if event.BlockNumber < chainHeight {
 		return false
 	}
 	return true
 }
+
+func (registry *SnapshotRegistry) notify(isSuccess bool, message string, extra ...interface{}) {
+
+}
+
