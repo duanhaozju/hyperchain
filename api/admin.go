d288a0fa186c46273ed04d21d4180a7c04f18bff
rjl493456442 <garyrong0905@gmail.com>
2017-05-24 19:53:26 +0800

    feat: add snapshot integrity check api

--- api/admin.go
+++ api/admin.go
@@ -5,6 +5,7 @@ import (
 	"hyperchain/common"
 	"hyperchain/manager/event"
 	flt "hyperchain/manager/filter"
+	edb "hyperchain/core/db_utils"
 )
 
 type AdminPublicAPI struct {
@@ -51,6 +52,7 @@ func (admin *AdminPublicAPI) ReadSnapshot(filterId string, verbose bool) (interf
 		if err != nil {
 			return nil, &common.SnapshotErr{Message: err.Error()}
 		}
+		// TODO use a more pretty display mode
 		return string(stateDb.Dump()), nil
 	}
 }
@@ -73,3 +75,29 @@ func (admin *AdminPublicAPI) DeleteSnapshot(filterId string) string {
 	return filterId
 }
 // TODO delete snapshot callback
+
+func (admin *AdminPublicAPI) CheckSnapshot(filterId string) (bool, error) {
+	manifestHandler := common.NewManifestHandler(getManifestPath(admin.config))
+	var manifest common.Manifest
+	var err error
+	if err, manifest = manifestHandler.Read(filterId); err != nil {
+		return false, &common.SnapshotErr{Message: err.Error()}
+	}
+	// return whole world state
+	stateDb, err := NewSnapshotStateDb(admin.config, manifest.FilterId, common.Hex2Bytes(manifest.MerkleRoot), manifest.Height, manifest.Namespace)
+	if err != nil {
+		return false, &common.SnapshotErr{Message: err.Error()}
+	}
+	curHash, err := stateDb.RecomputeCryptoHash()
+	if err != nil {
+		return false, &common.SnapshotErr{Message: err.Error()}
+	}
+	blk, err := edb.GetBlockByNumber(admin.namespace, manifest.Height)
+	if err != nil {
+		return false, &common.SnapshotErr{Message: err.Error()}
+	}
+	if curHash != common.HexToHash(manifest.MerkleRoot) || curHash != common.BytesToHash(blk.MerkleRoot) {
+		return false, nil
+	}
+	return true, nil
+}
