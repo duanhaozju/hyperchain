49216b6f05cb3f386f751f5e313e86c222fb1cdb
Xiaoyi Wang <wangxiaoyi@hyperchain.cn>
2017-06-05 13:39:50 +0800

    Fix Bug #898: concurrent map read and map write related to config

--- common/config.go
+++ common/config.go
@@ -3,19 +3,24 @@
 package common
 
 import (
-	"fmt"
 	"github.com/spf13/viper"
-	"os"
+
+	"fmt"
 	"time"
+	"os"
+	"github.com/fsnotify/fsnotify"
+	"sync"
 )
 
 type Config struct {
 	conf *viper.Viper
+	lock *sync.RWMutex
 }
 
 //NewConfig return a new instance of Config by configPath
 func NewConfig(configPath string) *Config {
 	vp := viper.New()
+	vp.WatchConfig()
 	vp.SetConfigFile(configPath)
 	err := vp.ReadInConfig()
 	if err != nil {
@@ -23,47 +28,68 @@ func NewConfig(configPath string) *Config {
 	}
 	return &Config{
 		conf: vp,
+		lock: &sync.RWMutex{},
 	}
 }
 
 func (cf *Config) Get(key string) interface{} {
+	cf.lock.RLock()
+	defer cf.lock.RUnlock()
 	return cf.conf.Get(key)
 }
 
 func (cf *Config) GetString(key string) string {
+	cf.lock.RLock()
+	defer cf.lock.RUnlock()
 	return cf.conf.GetString(key)
 }
 
 func (cf *Config) GetInt(key string) int {
+	cf.lock.RLock()
+	defer cf.lock.RUnlock()
 	return cf.conf.GetInt(key)
 }
 
 func (cf *Config) GetInt64(key string) int64 {
+	cf.lock.RLock()
+	defer cf.lock.RUnlock()
 	return cf.conf.GetInt64(key)
 }
 
 func (cf *Config) GetFloat64(key string) float64 {
+	cf.lock.RLock()
+	defer cf.lock.RUnlock()
 	return cf.conf.GetFloat64(key)
 }
 
 func (cf *Config) GetBool(key string) bool {
+	cf.lock.RLock()
+	defer cf.lock.RUnlock()
 	return cf.conf.GetBool(key)
 }
 
 func (cf *Config) GetDuration(key string) time.Duration {
+	cf.lock.RLock()
+	defer cf.lock.RUnlock()
 	return cf.conf.GetDuration(key)
 }
 
 func (cf *Config) GetStringMap(key string) map[string]interface{} {
+	cf.lock.RLock()
+	defer cf.lock.RUnlock()
 	return cf.conf.GetStringMap(key)
 }
 
 func (cf *Config) Set(key string, value interface{}) {
+	cf.lock.Lock()
+	defer cf.lock.Unlock()
 	cf.conf.Set(key, value)
 }
 
 // ContainsKey judge whether the key is set in the config
 func (cf *Config) ContainsKey(key string) bool {
+	cf.lock.RLock()
+	defer cf.lock.RUnlock()
 	return cf.conf.IsSet(key)
 }
 
@@ -78,3 +104,8 @@ func (cf *Config) MergeConfig(configPath string) (*Config, error) {
 	cf.conf.MergeConfig(f)
 	return cf, nil
 }
+//OnConfigChange register function to invoke when config file change
+func (cf *Config) OnConfigChange(run func(in fsnotify.Event))  {
+	cf.conf.OnConfigChange(run)
+}
+
