52c4502dc27d7183c9f66ca1a85026793d0a5dfe
Xiaoyi Wang <wangxiaoyi@hyperchain.cn>
2017-06-07 18:52:02 +0800

    Throw exception when parse value while value is empty

--- core/vm/jcee/java/src/main/java/cn/hyperchain/jcee/ledger/Result.java
+++ core/vm/jcee/java/src/main/java/cn/hyperchain/jcee/ledger/Result.java
@@ -1,5 +1,6 @@
 package cn.hyperchain.jcee.ledger;
 
+import cn.hyperchain.jcee.common.exception.ResultNotExistException;
 import cn.hyperchain.jcee.util.Bytes;
 import com.google.protobuf.ByteString;
 
@@ -14,34 +15,58 @@ public class Result {
     }
 
     public boolean toBoolean() {
+        if (this.isEmpty()) {
+            throw new ResultNotExistException();
+        }
         return Boolean.parseBoolean(value.toStringUtf8());
     }
 
     public char toChar() {
+        if (this.isEmpty()) {
+            throw new ResultNotExistException();
+        }
         return value.toStringUtf8().charAt(0);
     }
 
     public short toShort() {
+        if (this.isEmpty()) {
+            throw new ResultNotExistException();
+        }
         return Short.parseShort(value.toStringUtf8());
     }
 
-    public int toInt() {
+    public int toInt()  {
+        if (this.isEmpty()) {
+            throw new ResultNotExistException();
+        }
         return Integer.parseInt(value.toStringUtf8());
     }
 
     public float toFloat() {
+        if (this.isEmpty()) {
+            throw new ResultNotExistException();
+        }
         return Float.parseFloat(value.toStringUtf8());
     }
 
     public double toDouble() {
+        if (this.isEmpty()) {
+            throw new ResultNotExistException();
+        }
         return Double.parseDouble(value.toStringUtf8());
     }
 
-    public String transferToString() {
+    public String toString() {
+        if (this.isEmpty()) {
+            throw new ResultNotExistException();
+        }
         return value.toStringUtf8();
     }
 
-    public <T> T toObeject(Class<T> clazz){
+    public <T> T toObeject(Class<T> clazz) {
+        if (this.isEmpty()) {
+            throw new ResultNotExistException();
+        }
         return Bytes.toObject(value.toByteArray(), clazz);
     }
 
@@ -50,6 +75,16 @@ public class Result {
     }
 
     public ByteString getValue() {
+        if (this.isEmpty()) {
+            throw new ResultNotExistException();
+        }
         return value;
+    }
+
+    public byte[] toBytes() {
+        if (this.isEmpty()) {
+            throw new ResultNotExistException();
+        }
+        return value.toByteArray();
     }
 }
