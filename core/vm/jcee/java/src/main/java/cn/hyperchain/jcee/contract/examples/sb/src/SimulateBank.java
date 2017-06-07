e97552cb21bec506d7d26d53153f724fd9ecf2c9
Xiaoyi Wang <wangxiaoyi@hyperchain.cn>
2017-06-07 19:31:05 +0800

    Redesign contract directory

--- core/vm/jcee/java/src/main/java/cn/hyperchain/jcee/contract/examples/sb/src/SimulateBank.java
+++ core/vm/jcee/java/src/main/java/cn/hyperchain/jcee/contract/examples/sb/src/SimulateBank.java
@@ -2,7 +2,7 @@
  * Hyperchain License
  * Copyright (C) 2016 The Hyperchain Authors.
  */
-package cn.hyperchain.jcee.contract.examples.sb;
+package cn.hyperchain.jcee.contract.examples.sb.src;
 
 import cn.hyperchain.jcee.common.ExecuteResult;
 import cn.hyperchain.jcee.contract.ContractTemplate;
