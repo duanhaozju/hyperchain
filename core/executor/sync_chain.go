f4f62ac96f90c9529e414dc8b5d8539720748fb2
rjl493456442 <garyrong0905@gmail.com>
2017-06-28 14:15:05 +0800

    core/executor: use tx version as a flag when persist receipt
    Motivations:
        Since node can retrieve blocks via state update. While those blocks
        may has the old version. So, during the tx/receipt persistence, we
        need to store the original version tag.

--- core/executor/sync_chain.go
+++ core/executor/sync_chain.go
@@ -154,7 +154,7 @@ func (executor *Executor) applyBlock(block *types.Block, seqNo uint64) (error, *
 	if err := executor.persistTransactions(batch, block.Transactions, seqNo); err != nil {
 		return err, nil
 	}
-	if err := executor.persistReceipts(batch, result.Receipts, seqNo, common.BytesToHash(block.BlockHash)); err != nil {
+	if err := executor.persistReceipts(batch, block.Transactions, result.Receipts, seqNo, common.BytesToHash(block.BlockHash)); err != nil {
 		return err, nil
 	}
 	return nil, result
