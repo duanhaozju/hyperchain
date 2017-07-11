0c95718ffbad41dbed96df7363f1a1b9667089fd
sammy <mamxxim@163.com>
2017-07-11 20:57:38 +0800

    delete useless function

--- api/jsonrpc/core/server.go
+++ api/jsonrpc/core/server.go
@@ -16,12 +16,19 @@ import (
 
 const (
 	stopPendingRequestTimeout             = 3 * time.Second // give pending requests stopPendingRequestTimeout the time to finish when the server is stopped
-	OptionMethodInvocation    CodecOption = 1 << iota       // OptionMethodInvocation is an indication that the codec supports RPC method calls
 	adminService                          = "admin"
 )
 
 // CodecOption specifies which type of messages this codec supports
 type CodecOption int
+
+const (
+	// OptionMethodInvocation is an indication that the codec supports RPC method calls
+	OptionMethodInvocation CodecOption = 1 << iota
+
+	// OptionSubscriptions is an indication that the codec suports RPC notifications
+	OptionSubscriptions = 1 << iota // support pub sub
+)
 
 // NewServer will create a new server instance with no registered handlers.
 func NewServer(nr namespace.NamespaceManager, stopHyperchain chan bool, restartHp chan bool) *Server {
--- api/jsonrpc/core/utils.go
+++ api/jsonrpc/core/utils.go
@@ -3,11 +3,8 @@
 package jsonrpc
 
 import (
-	"golang.org/x/net/context"
 	"math/big"
 	"reflect"
-	"unicode"
-	"unicode/utf8"
 	"encoding/json"
 	"strings"
 	"errors"
@@ -21,51 +18,6 @@ func splitRawMessage(args json.RawMessage) ([]string, error) {
 	str = str[2 : len(str)-2]
 	splitstr := strings.Split(str, ",")
 	return splitstr, nil
-}
-
-// Is this an exported - upper case - name?
-func isExported(name string) bool {
-	rune, _ := utf8.DecodeRuneInString(name)
-	return unicode.IsUpper(rune)
-}
-
-// Is this type exported or a builtin?
-func isExportedOrBuiltinType(t reflect.Type) bool {
-	for t.Kind() == reflect.Ptr {
-		t = t.Elem()
-	}
-	// PkgPath will be non-empty even for an exported type,
-	// so we need to check the type name as well.
-	return isExported(t.Name()) || t.PkgPath() == ""
-}
-
-var contextType = reflect.TypeOf((*context.Context)(nil)).Elem()
-
-// isContextType returns an indication if the given t is of context.Context or *context.Context type
-func isContextType(t reflect.Type) bool {
-	for t.Kind() == reflect.Ptr {
-		t = t.Elem()
-	}
-	return t == contextType
-}
-
-var errorType = reflect.TypeOf((*error)(nil)).Elem()
-
-// Implements this type the error interface
-func isErrorType(t reflect.Type) bool {
-	for t.Kind() == reflect.Ptr {
-		t = t.Elem()
-	}
-	return t.Implements(errorType)
-}
-
-// formatName will convert to first character to lower case
-func formatName(name string) string {
-	ret := []rune(name)
-	if len(ret) > 0 {
-		ret[0] = unicode.ToLower(ret[0])
-	}
-	return string(ret)
 }
 
 var bigIntType = reflect.TypeOf((*big.Int)(nil)).Elem()
