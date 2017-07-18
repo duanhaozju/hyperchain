f5c35e3c086bc5c8266af4132b28a87fa977b23f
chenquan <quan.chen@hyperchain.cn>
2017-07-18 22:13:13 +0800

    feature: add nvp delete

---
+++ p2p/msg/handler_nvp_delete.go
@@ -0,0 +1,46 @@
+package msg
+
+import (
+	pb "hyperchain/p2p/message"
+	"hyperchain/manager/event"
+	"fmt"
+)
+
+type NVPAttendMsgHandler struct {
+	mchan chan  interface{}
+	ev *event.TypeMux
+}
+
+func NewNVPAttendHandler(blackHole chan interface{},ev *event.TypeMux)*NVPAttendMsgHandler{
+	return &NVPAttendMsgHandler{
+		mchan:blackHole,
+		ev:ev,
+	}
+}
+
+//Process
+func (h  *NVPAttendMsgHandler) Process() {
+	for msg := range h.mchan {
+		 fmt.Println("got a Attend message", string(msg.(*pb.Message).Payload))
+	}
+}
+
+//Teardown
+func (h  *NVPAttendMsgHandler) Teardown() {
+	//TODO THIS is UN Allowed, because reciver cannot close the mchan
+	close(h.mchan)
+}
+
+//Receive
+func (h *NVPAttendMsgHandler)Receive() chan<- interface{}{
+	return h.mchan
+}
+
+//Execute
+func (h *NVPAttendMsgHandler)Execute(msg *pb.Message) (*pb.Message,error){
+	fmt.Printf("GOT A NVP ATTEND MSG hostname(%s), type: %s \n",msg.From.Hostname,msg.MessageType)
+	rsp  := &pb.Message{
+		MessageType:pb.MsgType_RESPONSE,
+	}
+	return rsp,nil
+}
