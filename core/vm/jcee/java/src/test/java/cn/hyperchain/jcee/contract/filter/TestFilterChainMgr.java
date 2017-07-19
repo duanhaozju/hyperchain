f8a71661ad53b5b2645c5841f78a0593216cd972
Xiaoyi Wang <wangxiaoyi@hyperchain.cn>
2017-07-19 14:24:00 +0800

    Update contract proto file

--- core/vm/jcee/java/src/test/java/cn/hyperchain/jcee/contract/filter/TestFilterChainMgr.java
+++ core/vm/jcee/java/src/test/java/cn/hyperchain/jcee/contract/filter/TestFilterChainMgr.java
@@ -5,39 +5,113 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by wangxiaoyi on 2017/7/18.
  */
 public class TestFilterChainMgr {
 
-    FilterChainMgr mgr;
+    static FilterChainMgr mgr;
 
     @BeforeClass
-    public void setup() {
+    public static void setup() {
         mgr = new FilterChainMgr();
     }
 
     @AfterClass
-    public void destroy() {
+    public static void destroy() {
 
     }
 
     @Test
     public void testAddGetFilterChain() {
         FilterChain fc = new FilterChain();
-        fc.addFilter(new TestFilter());
+        fc.addFilter(new NamespaceFilter());
 
     }
 
-    class TestFilter implements Filter{
+    class NamespaceFilter implements Filter{
+
+        private Set<String> permittedNamespaces;
+
+        protected void addPermittedNamespace(String namespace) {
+            permittedNamespaces.add(namespace);
+        }
+
+        public NamespaceFilter() {
+           permittedNamespaces = new HashSet<>();
+           permittedNamespaces.add("ns1");
+           permittedNamespaces.add("ns2");
+        }
+
         @Override
         public boolean doFilter(Context context) {
+            if (permittedNamespaces.contains(context.getRequestContext().getNamespace())) {
+                return true;
+            }
             return false;
         }
 
         @Override
         public String getName() {
-            return "TestFilter";
+            return "NamespaceFilter";
+        }
+    }
+
+
+    class ContractFilter implements Filter{
+
+        private Set<String> permittedContractAddrs;
+
+        protected void setPermittedContractAddrs(String contractAddr) {
+            permittedContractAddrs.add(contractAddr);
+        }
+
+        public ContractFilter() {
+            permittedContractAddrs = new HashSet<>();
+        }
+
+        @Override
+        public boolean doFilter(Context context) {
+            if (permittedContractAddrs.contains(context.getRequestContext().getCid())) {
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public String getName() {
+            return "ContractFilter";
+        }
+    }
+
+
+    class InvokerFilter implements Filter {
+
+        private Set<String> permittedInvokerAddrs;
+
+        protected void setPermittedInvokerAddrs(String invokerAddr) {
+            permittedInvokerAddrs.add(invokerAddr);
+        }
+
+        public InvokerFilter() {
+            permittedInvokerAddrs = new HashSet<>();
+            permittedInvokerAddrs.add("xxx");
+        }
+
+        @Override
+        public boolean doFilter(Context context) {
+            if (permittedInvokerAddrs.contains(context.getRequestContext().getInvoker())) {
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public String getName() {
+            return "InvokerFilter";
         }
     }
 }
