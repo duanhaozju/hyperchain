a09cf7e1d97a54c24d54a1ad74f8123363e98302
xucaichao <xucaichao@hyperchain.cn>
2017-07-31 21:34:56 +0800

    Modify contract_compile.sh

--- core/vm/jcee/java/bin/contract_compile.sh
+++ core/vm/jcee/java/bin/contract_compile.sh
@@ -5,7 +5,7 @@ if [ "$#" -ne 2 ]; then
     echo "invalid params length"
     echo "usage: contract_compile.sh contractDir contractClassDir"
 fi
-find $1 -name '*.java' | xargs javac -cp hyperjvm/sdk/hyperjvm-sdk-1.0.jar -d $2
+find $1 -name '*.java' | xargs javac -cp $(for i in ../libs/*.jar; do echo -n $i:; done) -d $2
 #javac -cp ../sdk/hyperjvm-sdk-1.0.jar -d
 
 #java -cp $(for i in ../libs/*.jar ; do echo -n $i: ; done).  $1 $2 $3
