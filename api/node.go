b26aa6d3a588716bcba0d3cf0da131e141d25402
sammy <mamxxim@163.com>
2017-08-01 15:22:48 +0800

    modify api method name. delete useless code

--- api/node.go
+++ api/node.go
@@ -50,26 +50,22 @@ func (node *Node) GetNodeHash() (string, error) {
 	return node.eh.GetPeerManager().GetLocalNodeHash(), nil
 }
 
-func (node *Node) DelNode(args NodeArgs) (string, error) {
+func (node *Node) DeleteVP(args NodeArgs) (string, error) {
 	if node.eh == nil {
 		return "", &common.CallbackError{Message:"protocolManager is nil"}
 	}
 	go node.eh.GetEventObject().Post(event.DelVPEvent{
 		Payload: []byte(args.NodeHash),
 	})
-	return "successful delete vp node", nil
+	return "successful request to delete vp node", nil
 }
 
-func (node *Node) DelNVP(args NodeArgs) (string, error) {
+func (node *Node) DeleteNVP(args NodeArgs) (string, error) {
 	if node.eh == nil {
 		return "", &common.CallbackError{Message:"protocolManager is nil"}
 	}
 	go node.eh.GetEventObject().Post(event.DelNVPEvent{
 		Payload: []byte(args.NodeHash),
 	})
-	return "successful delete nvp node", nil
-}
-
-func (node *Node) GetNamespace() (string, error) {
-	return node.namespace, nil
+	return "successful request to delete nvp node", nil
 }
