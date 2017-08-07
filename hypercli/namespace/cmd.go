0193f197865d4102b143e5c53bd016e17d704bd3
Xiaoyi Wang <wangxiaoyi@hyperchain.cn>
2017-08-07 14:55:36 +0800

    Add namespace id generate command

--- hypercli/namespace/cmd.go
+++ hypercli/namespace/cmd.go
@@ -6,7 +6,13 @@ import (
 	"fmt"
 	"github.com/urfave/cli"
 	"hyperchain/hypercli/common"
+	cm "hyperchain/common"
 	admin "hyperchain/api/jsonrpc/core"
+	"path"
+	"hyperchain/crypto"
+	"time"
+	"strconv"
+	"math/rand"
 )
 
 //NewNamespaceCMD new namespace related
@@ -21,6 +27,18 @@ func NewNamespaceCMD() []cli.Command {
 			Name:   "stopNsMgr",
 			Usage:  "stop the namespace manager",
 			Action: stop,
+		},
+		{
+			Name:   "new",
+			Usage:  "generate a unique namespace id",
+			Action: new,
+			Flags:  []cli.Flag{
+				cli.StringFlag{
+					Name:  "configDir, d",
+					Value: "",
+					Usage: "specify the namespace's config dir",
+				},
+			},
 		},
 		{
 			Name:   "start",
@@ -75,6 +93,48 @@ func stop(c *cli.Context) error {
 	result := client.InvokeCmd(cmd)
 	fmt.Print(result)
 	return nil
+}
+
+func new(c *cli.Context) error  {
+	fns := c.FlagNames()
+	for _, fn := range fns {
+
+		if fn == "configDir" {
+			dir := c.String(fn)
+			if len(dir) == 0 {
+				fmt.Printf("%s is invalid namespace config dir!", dir)
+			}
+			conf := cm.NewConfig(path.Join(dir, "peerconfig.toml"))
+			nodes := conf.Get("nodes")
+			var hostNames string
+			if nodemap, ok := nodes.([]interface{}); ok {
+				for _, n := range nodemap {
+					if node, ok := n.(map[string]interface{}); ok {
+						fmt.Printf("node name %s, node value %s \n", "hostname", node["hostname"])
+						hostName, _ := node["hostname"].(string)
+						hostNames += hostName
+					}else {
+						fmt.Println("invalid peerconfig")
+					}
+				}
+				name := generateNamespaceId(hostNames)
+				fmt.Printf("new namespace id is: %s\n", name)
+			}else {
+				fmt.Println("invalid peerconfig")
+			}
+		}
+	}
+	return nil
+}
+
+//namespace generate functions
+func generateNamespaceId(hostNames string) string  {
+	kec256Hash := crypto.NewKeccak256Hash("keccak256")
+	hashString := hostNames + time.Now().String() + strconv.Itoa(rand.Intn(1000000))
+	fmt.Println(hashString)
+	hash := kec256Hash.Hash(hashString).Hex()
+	fmt.Println(hash)
+	return "ns_" + hash[2:14]
 }
 
 func startNamespace(c *cli.Context) error {
