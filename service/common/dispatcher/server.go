6d019b087b5f71c420002987d39517f76c67e73e
Xiaoyi Wang <wangxiaoyi@hyperchain.cn>
2017-09-25 16:01:38 +0800

    Service registry impl

--- service/common/dispatcher/server.go
+++ service/common/dispatcher/server.go
@@ -1,1 +1,34 @@
 package dispatcher
+
+import (
+	"hyperchain/service/common"
+	pb "hyperchain/service/common/protos"
+)
+
+//DispatchServer dispatch service
+type DispatchServer struct {
+	port int
+	host string
+	sr   common.ServiceRegistry
+}
+
+func NewDispatchServer(port int, host string) (*DispatchServer, error) {
+	ds := &DispatchServer{
+		port: port,
+		host: host,
+	}
+	return ds, nil
+}
+
+//Register receive a new connection
+func (ds *DispatchServer) Register(stream pb.Dispatcher_RegisterServer) error {
+
+	return nil
+}
+
+//dispatch dispatch messages
+func (ds *DispatchServer) dispatch() {
+
+}
+
+
