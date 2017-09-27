32e32a5616b27856489193073c6f39bf4199274d
Xiaoyi Wang <wangxiaoyi@hyperchain.cn>
2017-09-27 11:12:39 +0800

    Add dispatch message skeleton

---
+++ service/common/dispatcher/dispatch.go
@@ -0,0 +1,21 @@
+package dispatcher
+
+import (
+	pb "hyperchain/service/common/protos"
+)
+
+func (ds *DispatchServer) dispatchConsensusMsg(msg *pb.Message) {
+	
+}
+
+func (ds *DispatchServer) dispatchExecutorMsg(msg *pb.Message) {
+	
+}
+
+func (ds *DispatchServer) dispatchNetworkMsg(msg *pb.Message) {
+
+}
+
+func (ds *DispatchServer) dispatchAPIServerMsg(msg *pb.Message) {
+
+}
