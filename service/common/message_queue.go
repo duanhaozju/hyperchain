52accabfb92393c5479a8753b3ed242db5e2e78c
Xiaoyi Wang <wangxiaoyi@hyperchain.cn>
2017-10-09 13:29:43 +0800

    Change message type

--- service/common/message_queue.go
+++ service/common/message_queue.go
@@ -1,31 +1,33 @@
 package common
 
+import (
+	pb "hyperchain/service/common/protos"
+)
+
 //MessageQueue queue to store messages.
-type MessageQueue interface{
-	Put(msg interface{}) error
-	Get() (interface{}, error)
+type MessageQueue interface {
+	Put(msg *pb.Message) error
+	Get() (*pb.Message, error)
 }
 
 //MQImpl message queue implementation.
 type MQImpl struct {
-	messages chan interface{}
+	messages chan *pb.Message
 }
 
 func newMQImpl(size int) *MQImpl {
 	mq := &MQImpl{
-		messages:make(chan interface{}, size),
+		messages: make(chan *pb.Message, size),
 	}
 	return mq
 }
 
-func (mq *MQImpl)Put(msg interface{}) error  {
+func (mq *MQImpl) Put(msg *pb.Message) error {
 	mq.messages <- msg
 	return nil
 }
 
-func (mq *MQImpl)Get() (interface{}, error)  {
-	msg := mq.messages
+func (mq *MQImpl) Get() (*pb.Message, error) {
+	msg := <-mq.messages
 	return msg, nil
 }
-
-
