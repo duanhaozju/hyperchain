9c451d039b6025ee109ca6fc73740058eb035afd
Zhiqiang Wu <wuzhiqiang@hyperchain.cn>
2017-10-10 20:31:48 +0800

    consensus: add unitest about helper.go

--- consensus/rbft/rbft_test_util_test.go
+++ consensus/rbft/rbft_test_util_test.go
@@ -1,13 +1,13 @@
 package rbft
 
 import (
-	"testing"
-	"time"
-	"github.com/facebookgo/ensure"
+	//"testing"
+	//"time"
+	//"github.com/facebookgo/ensure"
 )
 
-func TestPbftImpl_func2(t *testing.T) {
-	pbftList:= CreatRBFT(t,4,"./Testdatabase/","../../configuration/namespaces/","global",nil)
-	time.Sleep(2*time.Second)
-	ensure.DeepEqual(t,0,int(pbftList[1].status.inRecovery))
-}
+//func TestPbftImpl_func2(t *testing.T) {
+//	pbftList:= CreatRBFT(t,4,"./Testdatabase/","../../configuration/namespaces/","global",nil)
+//	time.Sleep(2*time.Second)
+//	ensure.DeepEqual(t,0,int(pbftList[1].status.inRecovery))
+//}
