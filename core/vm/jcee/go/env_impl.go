f245aadbb3dd9353b064fd1b3b1036149e878648
rjl493456442 <garyrong0905@gmail.com>
2017-10-10 15:18:42 +0800

    core/vm/evm: add ethereum licenses

--- core/vm/jcee/go/env_impl.go
+++ core/vm/jcee/go/env_impl.go
@@ -9,37 +9,26 @@ import (
 	"math/big"
 )
 
-type Account struct {
-	Balance string
-	Code    string
-	Nonce   string
-	Storage map[string]string
-}
-
 type Env struct {
-	depth      int
-	state      vm.Database
-	Gas        *big.Int
-	origin     common.Address
-	coinbase   common.Address
-	number     *big.Int
-	time       *big.Int
-	difficulty *big.Int
-	gasLimit   *big.Int
-	logs       []*types.Log
-	namespace  string
-	txHash     common.Hash
-	logger     *logging.Logger
-	jvm        vm.Vm
+	depth     int
+	state     vm.Database
+	Gas       *big.Int
+	origin    common.Address
+	number    *big.Int
+	time      *big.Int
+	logs      []*types.Log
+	namespace string
+	txHash    common.Hash
+	logger    *logging.Logger
+	jvm       vm.Vm
 }
 
-func NewEnv(state vm.Database, setting map[string]string, logger *logging.Logger, namespace string, txHash common.Hash, jvmCli ContractExecutor) *Env {
+func NewEnv(state vm.Database, number, timestamp int64, logger *logging.Logger, namespace string, txHash common.Hash, jvmCli ContractExecutor) *Env {
 	env := &Env{
 		state:     state,
 		logger:    logger,
-		time:      common.Big(setting["currentTimestamp"]),
-		gasLimit:  common.Big(setting["currentGasLimit"]),
-		number:    common.Big(setting["currentNumber"]),
+		time:      big.NewInt(timestamp),
+		number:    big.NewInt(number),
 		namespace: namespace,
 		txHash:    txHash,
 		Gas:       new(big.Int),
@@ -48,42 +37,24 @@ func NewEnv(state vm.Database, setting map[string]string, logger *logging.Logger
 	return env
 }
 
-func (self *Env) Vm() vm.Vm              { return self.jvm }
-func (self *Env) Origin() common.Address { return self.origin }
-func (self *Env) BlockNumber() *big.Int  { return self.number }
-
-func (self *Env) Time() *big.Int { return self.time }
-
-func (self *Env) Db() vm.Database { return self.state }
-
-func (self *Env) VmType() vm.Type         { return vm.JavaVmTy }
-func (self *Env) Logger() *logging.Logger { return self.logger }
-func (self *Env) Namespace() string       { return self.namespace }
-
-func (self *Env) TransactionHash() common.Hash {
-	return self.txHash
-}
-
+func (self *Env) Vm() vm.Vm                    { return self.jvm }
+func (self *Env) Origin() common.Address       { return self.origin }
+func (self *Env) BlockNumber() *big.Int        { return self.number }
+func (self *Env) Time() *big.Int               { return self.time }
+func (self *Env) Db() vm.Database              { return self.state }
+func (self *Env) VmType() vm.Type              { return vm.JavaVmTy }
+func (self *Env) Logger() *logging.Logger      { return self.logger }
+func (self *Env) Namespace() string            { return self.namespace }
+func (self *Env) TransactionHash() common.Hash { return self.txHash }
 func (self *Env) GetHash(n uint64) common.Hash {
 	return common.BytesToHash(crypto.Keccak256([]byte(big.NewInt(int64(n)).String())))
 }
-
-func (self *Env) AddLog(log *types.Log) {
-	self.Db().AddLog(log)
-}
-
-func (self *Env) DumpStructLog() {
-	// TODO
-}
-func (self *Env) Depth() int     { return self.depth }
-func (self *Env) SetDepth(i int) { self.depth = i }
-
-func (self *Env) MakeSnapshot() interface{} {
-	return self.state.Snapshot()
-}
-func (self *Env) SetSnapshot(copy interface{}) {
-	self.state.RevertToSnapshot(copy)
-}
+func (self *Env) AddLog(log *types.Log)        { self.Db().AddLog(log) }
+func (self *Env) DumpStructLog()               {}
+func (self *Env) Depth() int                   { return self.depth }
+func (self *Env) SetDepth(i int)               { self.depth = i }
+func (self *Env) MakeSnapshot() interface{}    { return self.state.Snapshot() }
+func (self *Env) SetSnapshot(copy interface{}) { self.state.RevertToSnapshot(copy) }
 
 // Call java based contract invocation.
 func (self *Env) Call(caller vm.ContractRef, addr common.Address, data []byte, gas, price, value *big.Int, op int32) ([]byte, error) {
