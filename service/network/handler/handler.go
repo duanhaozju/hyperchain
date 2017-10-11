c8b336151edc487b61678cc75d8723385d40a554
Xiaoyi Wang <wangxiaoyi@hyperchain.cn>
2017-10-11 14:46:35 +0800

    Add close mechanism for service client

--- service/network/handler/handler.go
+++ service/network/handler/handler.go
@@ -24,6 +24,7 @@ func New() common.Handler {
 func (nmh *NetworkMessageHandler) Handle(msg *pb.Message) {
 	switch msg.Event {
 	case pb.Event_InformPrimaryEvent:
+		logger.Error(msg)
 		event := &event.InformPrimaryEvent{}
 		err := proto.Unmarshal(msg.Payload, event)
 		if err != nil {
