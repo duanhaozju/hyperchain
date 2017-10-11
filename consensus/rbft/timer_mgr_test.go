d9ceff2dbe0dcdada1ee278d852d4a210b413338
Zhiqiang Wu <wuzhiqiang@hyperchain.cn>
2017-10-11 20:07:52 +0800

    consensus: add unitest about timer_mgr.go

--- consensus/rbft/timer_mgr_test.go
+++ consensus/rbft/timer_mgr_test.go
@@ -1,44 +1,54 @@
+//Hyperchain License
+//Copyright (C) 2016 The Hyperchain Authors.
+
 package rbft
 
-//func TestPbftTimeFunctions(t *testing.T)  {
-//	pp := new (pbftImpl)
-//	pp.batchTimeout = 1 * time.Second
-//	pp.batchTimerActive = false;
-//	pp.batchTimer = events.NewTimerFactoryImpl(events.NewManagerImpl()).CreateTimer()
-//
-//	pp.startBatchTimer()
-//	if(pp.batchTimeout != 1 * time.Second || pp.batchTimerActive == false){
-//		t.Errorf("pbftProtocal %s not work!", "startBatchTimer")
-//	}
-//
-//	pp.stopBatchTimer()
-//	if pp.batchTimerActive == true {
-//		t.Errorf("pbftProtocal %s not work!", "stopBatchTimer")
-//	}
-//
-//	pp.newViewTimer =  events.NewTimerFactoryImpl(events.NewManagerImpl()).CreateTimer()
-//	pp.startNewViewTimer(2 * time.Second, "test pbftProtocol viewTimer")
-//	if pp.timerActive == false {
-//		t.Errorf("pbftProtocal %s not work!", "startTimer")
-//	}
-//
-//	pp.stopNewViewTimer()
-//	if pp.timerActive == true {
-//		t.Errorf("pbftProtocal %s not work!", "stopTimer")
-//	}
-//	rs := "test pbftProtocol softSDtartTimer"
-//	pp.softStartTimer(1 * time.Second, rs)
-//	if pp.timerActive == false || strings.Compare(pp.newViewTimerReason, rs) != 0 {
-//		t.Errorf(`pbftProtocal %s not work!`, "softStartTimer")
-//	}
-//
-//	/*pp.nullRequestTimer = events.NewTimerFactoryImpl(events.NewManagerImpl()).CreateTimer()
-//	pp.nullRequestTimeout = 2 * time.Second
-//	pp.id = 1
-//	pp.view = 1
-//	pp.requestTimeout = 3
-//
-//
-//	pp.nullReqTimerReset()
-//	pp.nullRequestTimer*/
-//}
+import (
+	"time"
+	"testing"
+
+	"hyperchain/common"
+	"hyperchain/consensus/helper/persist"
+	mdb "hyperchain/hyperdb/mdb"
+	"hyperchain/manager/event"
+
+	"github.com/stretchr/testify/assert"
+
+)
+
+func TestPbftTimeFunctions(t *testing.T)  {
+	ast := assert.New(t)
+	rbft, _, err := TNewRbft("./Testdatabase/", "../../configuration/namespaces/", "global", 2, t)
+	defer CleanData(rbft.namespace)
+	ast.Equal(nil, err, err)
+	rbft.Start()
+	db, err := mdb.NewMemDatabase(common.DEFAULT_NAMESPACE)
+	rbft.persister = persist.New(db)
+
+	timerName := "test"
+	timerDuration := 5 * time.Second
+	rbft.timerMgr.newTimer(timerName, timerDuration)
+
+	ok := rbft.timerMgr.containsTimer(timerName)
+	ast.Equal(true, ok, "newTimer failed")
+	ast.Equal(timerDuration, rbft.timerMgr.getTimeoutValue(timerName), "getTimeoutValue failed")
+	ast.Equal(0 * time.Second, rbft.timerMgr.getTimeoutValue("notexist"), "getTimeoutValue failed")
+	rbft.timerMgr.setTimeoutValue(timerName, 10 * time.Second)
+	rbft.timerMgr.setTimeoutValue("notexist", 10 * time.Second)
+	ast.Equal(10 * time.Second, rbft.timerMgr.getTimeoutValue(timerName), "setTimeoutValue failed")
+
+	queue := &event.TypeMux{}
+	rbft.timerMgr.startTimer(timerName, nil, queue)
+	ast.Equal(1, len(rbft.timerMgr.ttimers[timerName].isActive), "startTimer failed")
+	rbft.timerMgr.startTimerWithNewTT(timerName, 20 * time.Second, nil, queue)
+	ast.Equal(2, len(rbft.timerMgr.ttimers[timerName].isActive), "startTimer failed")
+
+	rbft.timerMgr.stopOneTimer(timerName, 1)
+	ast.Equal(false, rbft.timerMgr.ttimers[timerName].isActive[1], "stopOneTimer failed")
+
+	isActive := rbft.timerMgr.ttimers[timerName].isActive
+	rbft.timerMgr.stopOneTimer("notexist", 1)
+	ast.Equal(isActive, rbft.timerMgr.ttimers[timerName].isActive, "stopOneTimer failed")
+	rbft.timerMgr.stopOneTimer(timerName, 3)
+	ast.Equal(isActive, rbft.timerMgr.ttimers[timerName].isActive, "stopOneTimer failed")
+}
