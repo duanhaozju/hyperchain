952e110a93c8c06ba735276fd3c7d5ca80733b7a
heqi <heqi@hyperchain.cn>
2017-10-17 19:40:09 +0800

    add services in ecManagerImpl

--- service/executor/executor/ecmgr.go
+++ service/executor/executor/ecmgr.go
@@ -3,6 +3,7 @@ package executor
 import (
 	"hyperchain/common"
 	"hyperchain/core/executor"
+	"hyperchain/common/service"
 )
 
 type ExecutorManager interface {
@@ -17,31 +18,36 @@ type ecManagerImpl struct {
 
 	executors map[string]executor.Executor
 
+	// manager the connect, it can be included in the ExecutorRemote when it add
+	services map[string]service.Service
+
 	// conf is the global config file of the system, contains global configs
 	// of the node
 	conf *common.Config
 
-	stopHp    chan bool
-	restartHp chan bool
+	stopEm    chan bool
+	restartEm chan bool
 }
 
-func newExecutorManager(conf *common.Config, stopHp chan bool, restartHp chan bool) *ecManagerImpl {
+func newExecutorManager(conf *common.Config, stopEm chan bool, restartEm chan bool) *ecManagerImpl {
 	em := &ecManagerImpl{
 		executors:  make(map[string]executor.Executor),
 
 		conf:        conf,
-		stopHp:      stopHp,
-		restartHp:   restartHp,
+		stopEm:      stopEm,
+		restartEm:   restartEm,
 	}
 	return em
 }
 
-func GetExecutorMgr(conf *common.Config, stopHp chan bool, restartHp chan bool) *ecManagerImpl{
-	return newExecutorManager(conf, stopHp,restartHp)
+func GetExecutorMgr(conf *common.Config, stopEm chan bool, restartEM chan bool) *ecManagerImpl{
+	return newExecutorManager(conf, stopEm, restartEM)
 }
 
 func (em *ecManagerImpl) Start(){
 	//TODO: 读配置文件,开启多个executor实例
+
+
 }
 
 func (em *ecManagerImpl) Stop(){
