fc13aab6d5aff6d58606a5841357fa2bd179ae23
guowei <guowei@hyperchain.cn>
2017-10-18 21:47:44 +0800

    implement the service/executor/api/api.go, implements the start() and Stop() methods.

--- service/executor/apiserver/apiserver.go
+++ service/executor/apiserver/apiserver.go
@@ -1,11 +1,34 @@
 package apiserver
 
+import(
+	"hyperchain/rpc"
+	"hyperchain/namespace"
+	"hyperchain/common"
+)
+
 type APIServer interface {
-
 	Start()
-
 	Stop()
 
 }
 
+type APIServerImpl struct{
+	rpcServer *jsonrpc.RPCServerImpl
+}
+
+func NewAPIServer(nr namespace.NamespaceManager, config *common.Config) *APIServerImpl{
+	s := jsonrpc.GetRPCServer(nr, config)
+	apiserver := &APIServerImpl{
+		rpcServer: s,
+	}
+	return apiserver
+}
+
+func(asi *APIServerImpl) Start() error{
+	return asi.rpcServer.Start()
+}
+
+func(asi *APIServerImpl) Stop() error{
+	return asi.Stop()
+}
 // TODO : implement it
