67d914d466935266bfaf7a1e8203d12cb1e105bc
guowei <guowei@hyperchain.cn>
2017-10-30 20:40:39 +0800

    fix the origin code, which was deleted for test.

--- rpc/json.go
+++ rpc/json.go
@@ -17,6 +17,8 @@ import (
 	"strings"
 	"sync"
 	"hyperchain/common/interface"
+	"hyperchain/crypto/primitives"
+	"crypto/ecdsa"
 )
 
 const (
@@ -95,40 +97,40 @@ func (c *jsonCodecImpl) CheckHttpHeaders(namespace string, method string) common
 		return &common.NamespaceNotFound{Name: namespace}
 	}
 
-	//cm := ns.GetCAManager()
-	//if !cm.IsCheckTCert() {
-	//	return nil
-	//}
+	cm := ns.GetCAManager()
+	if !cm.IsCheckTCert() {
+		return nil
+	}
 
-	//c.decMu.Lock()
-	//defer c.decMu.Unlock()
-	//
-	//// parse certificate
-	//tcertPem := common.TransportDecode(c.req.Header.Get("tcert"))
-	//tcert, err := primitives.ParseCertificate([]byte(tcertPem))
-	//if err != nil {
-	//	log.Error("fail to parse tcert.", err)
-	//	return &common.UnauthorizedError{}
-	//}
-	//
-	//// verify signature
-	//pubKey := tcert.PublicKey.(*(ecdsa.PublicKey))
-	//signature := c.req.Header.Get("signature")
-	//msg := common.TransportDecode(c.req.Header.Get("msg"))
-	//signB := common.Hex2Bytes(signature)
-	//
-	//verifySignature, err := primitives.ECDSAVerifyTransport(pubKey, []byte(msg), signB)
-	//if err != nil || !verifySignature {
-	//	log.Error("Fail to verify Transport Signture!", err)
-	//	return &common.UnauthorizedError{}
-	//}
+	c.decMu.Lock()
+	defer c.decMu.Unlock()
 
-	//// verfiy tcert
-	//verifyTcert, err := cm.VerifyTCert(tcertPem, method)
-	//if verifyTcert == false || err != nil {
-	//	log.Error("Fail to verify tcert!", err)
-	//	return &common.UnauthorizedError{}
-	//}
+	// parse certificate
+	tcertPem := common.TransportDecode(c.req.Header.Get("tcert"))
+	tcert, err := primitives.ParseCertificate([]byte(tcertPem))
+	if err != nil {
+		log.Error("fail to parse tcert.", err)
+		return &common.UnauthorizedError{}
+	}
+
+	// verify signature
+	pubKey := tcert.PublicKey.(*(ecdsa.PublicKey))
+	signature := c.req.Header.Get("signature")
+	msg := common.TransportDecode(c.req.Header.Get("msg"))
+	signB := common.Hex2Bytes(signature)
+
+	verifySignature, err := primitives.ECDSAVerifyTransport(pubKey, []byte(msg), signB)
+	if err != nil || !verifySignature {
+		log.Error("Fail to verify Transport Signture!", err)
+		return &common.UnauthorizedError{}
+	}
+
+	// verfiy tcert
+	verifyTcert, err := cm.VerifyTCert(tcertPem, method)
+	if verifyTcert == false || err != nil {
+		log.Error("Fail to verify tcert!", err)
+		return &common.UnauthorizedError{}
+	}
 	return nil
 }
 
