2a6d572c830fd115562f1db32f73c30cc2efc043
zhangkejie <601172892@qq.com>
2017-11-01 21:38:13 +0800

    Add Bloom Event and add write event.

--- core/ledger/bloom/external.go
+++ core/ledger/bloom/external.go
@@ -3,6 +3,8 @@ package bloom
 import (
 	"hyperchain/common"
 	"hyperchain/core/types"
+	"github.com/pkg/errors"
+	"hyperchain/manager/event"
 )
 
 func LookupTransaction(namespace string, txHash common.Hash) (bool, error) {
@@ -17,4 +19,23 @@ func WriteTxBloomFilter(namespace string, txs []*types.Transaction) (bool, error
 		return false, ErrBloomCacheFailure
 	}
 	return bloomFilterCache.Write(namespace, txs)
+}
+
+type BloomFilterEvent struct {
+	Namespace    string               `protobuf:"bytes,1,opt,name=namespace" json:"namespace,omitempty"`
+	Transactions []*types.Transaction `protobuf:"bytes,2,rep,name=Transactions" json:"Transactions,omitempty"`
+}
+
+func WriteTxBloomFilterByEvent(ev event.BloomEvent) {
+	if bloomFilterCache == nil {
+		return false, ErrBloomCacheFailure
+	}
+	bol,err := bloomFilterCache.Write(ev.Namespace, ev.Transactions)
+	if !bol {
+		ev.Cont <- errors.New("Write failed!")
+	}
+	if err != nil {
+		ev.Cont <- err
+	}
+	close(ev.Cont)
 }
