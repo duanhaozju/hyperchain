b7688a63d523610999f8d070e031fb6cdd15c16d
sammy <mamxxim@163.com>
2017-11-09 14:30:44 +0800

    FIX: #1966

--- p2p/peerevent/event_deletenvp.go
+++ p2p/peerevent/event_deletenvp.go
@@ -1,5 +1,12 @@
 package peerevent
 
 type S_DELETE_NVP struct {
+
+	// hash of NVP peer will be deleted
 	Hash string
+
+	// hash of VP peer that NVP peer connected to.
+	// NVP peer should send a message to VP peer to disconnect through p2p network
+	// before NVP peer disconnect to VP peer.
+	VPHash string
 }
