51489146e3df4455ede72563e4eeab4594f34cc5
Zhiqiang Wu <wuzhiqiang@hyperchain.cn>
2017-11-21 15:22:09 +0800

    Add kvLogger implementation

--- core/oplog/kvlog/log.go
+++ core/oplog/kvlog/log.go
@@ -1,1 +1,71 @@
 package kvlog
+
+import (
+	"fmt"
+
+	"github.com/hyperchain/hyperchain/hyperdb/db"
+	"github.com/hyperchain/hyperchain/core/oplog/proto"
+
+	"github.com/golang/protobuf/proto"
+)
+
+
+type kvLoggerImpl struct {
+	lastSet	uint64
+	db		db.Database
+}
+
+func New(db db.Database) *kvLoggerImpl {
+	logger := &kvLoggerImpl{
+		db: db,
+	}
+	logger.lastSet = uint64(0)
+	return logger
+}
+
+func (logger *kvLoggerImpl) Append(entry *oplog.LogEntry) error {
+
+	if entry.Lid != logger.lastSet + 1 {
+		return fmt.Errorf("not matched with lastSet")
+	}
+
+	raw, err := proto.Marshal(entry)
+	if err != nil {
+		return err
+	}
+	key := fmt.Sprintf("entry.%d", logger.lastSet + 1)
+	if err = logger.db.Put([]byte(key), raw); err == nil {
+		logger.lastSet += 1
+	}
+	return err
+}
+
+func (logger *kvLoggerImpl) Fetch(lid uint64) (*oplog.LogEntry, error) {
+
+	if lid > logger.lastSet {
+		return nil, fmt.Errorf("lid is too large")
+	}
+
+	key := fmt.Sprintf("entry.%d", lid)
+	raw, err := logger.db.Get([]byte(key))
+	if err != nil {
+		return nil, err
+	}
+
+	entry := &oplog.LogEntry{}
+	if err = proto.Unmarshal(raw, entry); err != nil{
+		return nil, err
+	} else {
+		return entry, nil
+	}
+}
+
+func (logger *kvLoggerImpl) Reset(lid uint64) error {
+
+	if lid > logger.lastSet {
+		return fmt.Errorf("lid is too large")
+	}
+
+	logger.lastSet = lid
+	return nil
+}
---
+++ core/oplog/kvlog/log_test.go
@@ -0,0 +1,49 @@
+package kvlog
+
+import (
+
+	"testing"
+
+	"github.com/hyperchain/hyperchain/common"
+	mdb "github.com/hyperchain/hyperchain/hyperdb/mdb"
+	"github.com/hyperchain/hyperchain/core/oplog/proto"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLogAppendAndFetch(t *testing.T) {
+	ast := assert.New(t)
+	db, err := mdb.NewMemDatabase(common.DEFAULT_NAMESPACE)
+	ast.Equal(nil, err, err)
+	logger := New(db)
+
+	entry100 := &oplog.LogEntry{
+		Lid:	uint64(100),
+	}
+	err = logger.Append(entry100)
+	ast.NotNil(err, "lid is too large, should return an error")
+
+	entry1 := &oplog.LogEntry{
+		Lid:	uint64(1),
+		Type:	oplog.LogEntry_TransactionList,
+		Payload: []byte("1"),
+	}
+	err = logger.Append(entry1)
+	ast.Nil(err, "The appropriate lid, should not return any error")
+
+	entry2 , err := logger.Fetch(uint64(1))
+	ast.Equal(entry1, entry2, "should be equal")
+
+	entry2 = &oplog.LogEntry{
+		Lid:	uint64(2),
+		Type:	oplog.LogEntry_TransactionList,
+		Payload: []byte("2"),
+	}
+	err = logger.Append(entry2)
+	ast.Nil(err, "The appropriate lid, should not return any error")
+
+	err = logger.Reset(100)
+	ast.NotNil(err, "lid is too large, should return an error")
+	err = logger.Reset(1)
+	ast.Nil(err, "The appropriate lid, should not return any error")
+}
